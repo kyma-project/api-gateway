af677e3a3080596c62f365c3967a870451ca16ed
Karol Szwaj <karol.szwaj@gmail.com>
2022-11-30 13:48:46 +0100

    Remove old types

--- api/v1beta1/internal_types.go
+++
@@ -1,8 +0,0 @@
-package v1beta1
-
-// JWTAccStrConfig is used to deserialize jwt accessStrategy configuration for the validation purposes
-type JWTAccStrConfig struct {
-	TrustedIssuers []string `json:"trusted_issuers,omitempty"`
-	JWKSUrls       []string `json:"jwks_urls,omitempty"`
-	RequiredScopes []string `json:"required_scopes,omitempty"`
-}
