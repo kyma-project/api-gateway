1b7aeef7c0dd465033479c6dbc171c19416d0e0b
Karol Szwaj <karol.szwaj@gmail.com>
2022-12-01 17:08:47 +0100

    Add Istio JWT validator

--- internal/processing/istio/jwt_validator.go
+++ internal/processing/istio/jwt_validator.go
@@ -1,14 +1,48 @@
 package istio
 
 import (
+	"encoding/json"
+	"fmt"
+
 	gatewayv1beta1 "github.com/kyma-incubator/api-gateway/api/v1beta1"
+	istiojwt "github.com/kyma-incubator/api-gateway/internal/types/istio"
 	"github.com/kyma-incubator/api-gateway/internal/validation"
 )
 
 type jwtValidator struct{}
 
-func (i *jwtValidator) Validate(_ string, _ *gatewayv1beta1.Handler) []validation.Failure {
+func (o *jwtValidator) Validate(attributePath string, handler *gatewayv1beta1.Handler) []validation.Failure {
 	var problems []validation.Failure
+	var template istiojwt.JwtConfig
+
+	err := json.Unmarshal(handler.Config.Raw, &template)
+	if err != nil {
+		problems = append(problems, validation.Failure{AttributePath: attributePath + ".config", Message: "Can't read json: " + err.Error()})
+		return problems
+	}
+
+	for i, auth := range template.Authentications {
+		invalidIssuer, err := validation.IsInvalidURL(auth.Issuer)
+		if invalidIssuer {
+			attrPath := fmt.Sprintf("%s%s[%d]%s", attributePath, ".config.authentications", i, ".issuer")
+			problems = append(problems, validation.Failure{AttributePath: attrPath, Message: fmt.Sprintf("value is empty or not a valid url err=%s", err)})
+		}
+		unsecuredIssuer, err := validation.IsUnsecuredURL(auth.Issuer)
+		if unsecuredIssuer {
+			attrPath := fmt.Sprintf("%s%s[%d]%s", attributePath, ".config.authentications", i, ".issuer")
+			problems = append(problems, validation.Failure{AttributePath: attrPath, Message: fmt.Sprintf("value is not a secured url err=%s", err)})
+		}
+		invalidJwksUri, err := validation.IsInvalidURL(auth.JwksUri)
+		if invalidJwksUri {
+			attrPath := fmt.Sprintf("%s%s[%d]%s", attributePath, ".config.authentications", i, ".jwksUri")
+			problems = append(problems, validation.Failure{AttributePath: attrPath, Message: fmt.Sprintf("value is empty or not a valid url err=%s", err)})
+		}
+		unsecuredJwksUri, err := validation.IsUnsecuredURL(auth.JwksUri)
+		if unsecuredJwksUri {
+			attrPath := fmt.Sprintf("%s%s[%d]%s", attributePath, ".config.authentications", i, ".jwksUri")
+			problems = append(problems, validation.Failure{AttributePath: attrPath, Message: fmt.Sprintf("value is not a secured url err=%s", err)})
+		}
+	}
 
 	return problems
 }
