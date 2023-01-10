fb0bedbb778b19bf01114e6e01e2c4f64c33937a
Vladimir Videlov <vladimir.videlov@sap.com>
2023-01-10 15:16:45 +0100

    Webhook validation

--- api/v1beta1/apirule_webhook.go
+++ api/v1beta1/apirule_webhook.go
@@ -17,11 +17,54 @@ limitations under the License.
 package v1beta1
 
 import (
+	"context"
+	"errors"
+
+	runtime "k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
+var log = ctrl.Log.WithName("controllers").WithName("Api")
+
+var globalWebhookClient client.Client
+var globalWebhookContext context.Context
+
 func (r *APIRule) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	globalWebhookClient = mgr.GetClient()
+	globalWebhookContext = context.Background()
 	return ctrl.NewWebhookManagedBy(mgr).
 		For(r).
 		Complete()
+}
+
+// +kubebuilder:webhook:path=/mutate-gateway-kyma-project-io-v1beta1-apirule,mutating=true,failurePolicy=fail,groups=gateway.kyma-project.io,resources=apirules,verbs=create;update,versions=v1beta1,name=mapirule.kb.io,sideEffects=None,admissionReviewVersions=v1beta1
+
+var _ webhook.Defaulter = &APIRule{}
+
+// Default implements webhook.Defaulter so a webhook will be registered for the type
+func (r *APIRule) Default() {
+	log.Info("WEBHOOK->Default()")
+}
+
+// +kubebuilder:webhook:verbs=create;update,path=/validate-gateway-kyma-project-io-v1beta1-apirule,mutating=false,failurePolicy=fail,groups=gateway.kyma-project.io,resources=apirules,versions=v1beta1,name=vapirule.kb.io,admissionReviewVersions=v1beta1,sideEffects=None
+
+var _ webhook.Validator = &APIRule{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *APIRule) ValidateCreate() error {
+	log.Info("WEBHOOK->ValidateCreate()")
+	return errors.New("ErrorOnCreate")
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *APIRule) ValidateUpdate(old runtime.Object) error {
+	log.Info("WEBHOOK->ValidateUpdate()")
+	return errors.New("ErrorOnUpdate")
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *APIRule) ValidateDelete() error {
+	return nil
 }
