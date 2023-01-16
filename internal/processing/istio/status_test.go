733b7f15d63b50d4e3d78aedd6ec0157ba05cabf
Bartosz Chwila <103247439+barchw@users.noreply.github.com>
2023-01-16 13:01:21 +0100

    Handle status properly (#158)
    * Use status
    
    * Don't use selector for validation
    
    * Ignore reports
    
    * Format
    
    * Nil assert status
    
    * Use status base
    
    * Use strings
    
    * Debug kind
    
    * Unit test
    
    * Leave alphav1 as is
    
    * Remove unused
    
    * Revert name change
    
    * Change comment
    
    * Change comment
    
    * Extend BeforeSuite timeout
    
    * Descrive
    
    * gofmt
    
    * Fix tests
    
    * Move selector
    
    * Log
    
    * Log
    
    * Rule
    
    * Unit test
    
    * Update .gitignore
    
    * Add comment about missing Kind
    
    * Post review changes
    
    * Remove handler const

---
+++ internal/processing/istio/status_test.go
@@ -0,0 +1,20 @@
+package istio
+
+import (
+	gatewayv1beta1 "github.com/kyma-incubator/api-gateway/api/v1beta1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("IstioStatusBase", func() {
+	It("should create status base with AccessRule set to nil", func() {
+		// when
+		status := StatusBase(gatewayv1beta1.StatusSkipped)
+
+		Expect(status.ApiRuleStatus.Code).To(Equal(gatewayv1beta1.StatusSkipped))
+		Expect(status.VirtualServiceStatus.Code).To(Equal(gatewayv1beta1.StatusSkipped))
+		Expect(status.AuthorizationPolicyStatus.Code).To(Equal(gatewayv1beta1.StatusSkipped))
+		Expect(status.RequestAuthenticationStatus.Code).To(Equal(gatewayv1beta1.StatusSkipped))
+		Expect(status.AccessRuleStatus).To(BeNil())
+	})
+})
---
+++ internal/processing/ory/status_test.go
@@ -0,0 +1,20 @@
+package ory
+
+import (
+	gatewayv1beta1 "github.com/kyma-incubator/api-gateway/api/v1beta1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("OryStatusBase", func() {
+	It("should create status base with AP and RA set to nil", func() {
+		// when
+		status := StatusBase(gatewayv1beta1.StatusSkipped)
+
+		Expect(status.ApiRuleStatus.Code).To(Equal(gatewayv1beta1.StatusSkipped))
+		Expect(status.AccessRuleStatus.Code).To(Equal(gatewayv1beta1.StatusSkipped))
+		Expect(status.VirtualServiceStatus.Code).To(Equal(gatewayv1beta1.StatusSkipped))
+		Expect(status.AuthorizationPolicyStatus).To(BeNil())
+		Expect(status.RequestAuthenticationStatus).To(BeNil())
+	})
+})
