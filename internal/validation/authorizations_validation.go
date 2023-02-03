15dad4eb8687767135c0dc28d3c82f99705dceac
Bartosz Chwila <bartosz.chwila@sap.com>
2023-02-03 13:32:19 +0100

    Remove requirement for at least one scope

--- internal/validation/authorizations_validation.go
+++ internal/validation/authorizations_validation.go
@@ -6,9 +6,6 @@ import (
 )
 
 func HasInvalidScopes(authorization v1beta1.JwtAuthorization) error {
-	if len(authorization.RequiredScopes) == 0 {
-		return errors.New("value is empty")
-	}
 	for _, scope := range authorization.RequiredScopes {
 		if scope == "" {
 			return errors.New("scope value is empty")
