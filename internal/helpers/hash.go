da8516725d7fb8ac69bdfab59594d33f71d48aa1
Bartosz Chwila <bartosz.chwila@sap.com>
2023-02-27 15:22:22 +0100

    Add to to hash

--- internal/helpers/hash.go
+++ internal/helpers/hash.go
@@ -9,10 +9,19 @@ import (
 )
 
 func GetAuthorizationPolicyHash(ap securityv1beta1.AuthorizationPolicy) (string, error) {
-	hashTo, err := hashstructure.Hash(ap.Spec.Selector, hashstructure.FormatV2, &hashstructure.HashOptions{SlicesAsSets: true})
+	hashService, err := hashstructure.Hash(ap.Spec.Selector, hashstructure.FormatV2, &hashstructure.HashOptions{SlicesAsSets: true})
 	if err != nil {
 		return "", err
 	}
 
-	return fmt.Sprintf("%s.%s", ap.Namespace, strconv.FormatUint(hashTo, 32)), nil
+	var hashTo uint64
+	if len(ap.Spec.Rules) > 0 && ap.Spec.Rules[0].To != nil {
+		hash, err := hashstructure.Hash(ap.Spec.Rules[0].To, hashstructure.FormatV2, &hashstructure.HashOptions{SlicesAsSets: true})
+		if err != nil {
+			return "", err
+		}
+		hashTo = hash
+	}
+
+	return fmt.Sprintf("%s.%s.%s", ap.Namespace, strconv.FormatUint(hashService, 32), strconv.FormatUint(hashTo, 32)), nil
 }
