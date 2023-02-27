175cf2b50cced2a545bf0e5a2d6d92359f588f1d
Bartosz Chwila <bartosz.chwila@sap.com>
2023-02-27 08:56:43 +0100

    Hash to

--- internal/helpers/hash.go
+++ internal/helpers/hash.go
@@ -3,14 +3,18 @@ package helpers
 import (
 	"strconv"
 
-	securityv1beta1 "istio.io/client-go/pkg/apis/security/v1beta1"
 	"github.com/mitchellh/hashstructure/v2"
+	securityv1beta1 "istio.io/client-go/pkg/apis/security/v1beta1"
 )
 
 func GetAuthorizationPolicyHash(ap securityv1beta1.AuthorizationPolicy) (string, error) {
-	hash, err := hashstructure.Hash(ap.Spec, hashstructure.FormatV2, &hashstructure.HashOptions{SlicesAsSets: true})
+	hashTo, err := hashstructure.Hash(ap.Spec.Rules[0].To, hashstructure.FormatV2, &hashstructure.HashOptions{SlicesAsSets: true})
 	if err != nil {
 		return "", err
 	}
-	return strconv.FormatUint(hash, 32), nil
+	/*hashSpec, err := hashstructure.Hash(ap.Spec, hashstructure.FormatV2, &hashstructure.HashOptions{SlicesAsSets: true})
+	if err != nil {
+		return "", err
+	}*/
+	return strconv.FormatUint(hashTo, 32), nil
 }
