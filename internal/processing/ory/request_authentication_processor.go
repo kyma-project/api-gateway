a892295507559f666a84df47f4bc26817e87bd50
Tim Riffer <tim.riffer@sap.com>
2023-03-09 09:31:09 +0100

    Remove unused code

--- internal/processing/ory/request_authentication_processor.go
+++ internal/processing/ory/request_authentication_processor.go
@@ -7,17 +7,6 @@ import (
 	securityv1beta1 "istio.io/client-go/pkg/apis/security/v1beta1"
 )
 
-// RequestAuthenticationProcessor is the generic processor that handles the Istio Request Authentications in the reconciliation of API Rule.
-type RequestAuthenticationProcessor struct {
-	Creator requestAuthenticationCreator
-}
-
-// RequestAuthenticationCreator provides the creation of RequestAuthentications using the configuration in the given APIRule.
-// The key of the map is expected to be unique and comparable with the
-type RequestAuthenticationCreator interface {
-	Create(api *gatewayv1beta1.APIRule) map[string]*securityv1beta1.RequestAuthentication
-}
-
 // NewRequestAuthenticationProcessor returns a RequestAuthenticationProcessor with the desired state handling specific for the Istio handler.
 func NewRequestAuthenticationProcessor(config processing.ReconciliationConfig) processors.RequestAuthenticationProcessor {
 	return processors.RequestAuthenticationProcessor{
