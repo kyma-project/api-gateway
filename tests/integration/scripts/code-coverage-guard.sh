8b7ef72291fbaafa2c1c02610f23fc9cba10ff7e
Vladimir Videlov <vladimir.videlov@sap.com>
2023-03-29 16:02:14 +0200

    Code coverage guard (#277)
    * Code coverage guard
    
    * Debug
    
    * Script update
    
    * Desc updates
    
    * Fix env variable for api-gateway repo dir
    
    * Script path navigating
    
    * Use git pull instead of reset
    
    * git debug
    
    * Adapt cc check via awk
    
    * Final touches
    
    * Include PR and main commit ids
    
    * Adding new function without test
    
    * Check main on clean folder
    
    * Add test
    
    * Remove code duplication
    
    * Skip the test
    
    * Remove obsoletes
    
    * Remove commented test

---
+++ tests/integration/scripts/code-coverage-guard.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -eo pipefail
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+if [ -z "$PULL_PULL_SHA" ]; then
+  echo "Skipping code coverage guard execution, not on PR commit"
+  exit 0
+fi
+
+if [ -z "$PULL_BASE_SHA" ]; then
+  echo "Base commit for main not set as environment variable"
+  exit 1
+fi
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)"
+cd "${SCRIPT_DIR}/../../../" || exit 1
+
+echo -e "Code coverage guard (ensures PRs do not lower code coverage)"
+echo -e "Running tests on: PR-${PULL_NUMBER} (${PULL_PULL_SHA})"
+
+if ! make test; then
+	echo -e "${RED}✗ make test\\n${NC}"
+	exit 1
+else
+    coverage_pr=$(go tool cover -func=cover.out | grep total | awk '{print substr($3,1,length($3)-1)}')
+	echo -e "Total coverage on PR-${PULL_NUMBER}: ${coverage_pr}%"
+	echo -e "${GREEN}√ make test${NC}"
+    rm cover.out
+fi
+
+git checkout . && git clean -xffd
+git fetch && git checkout $PULL_BASE_SHA
+
+echo -e "Running tests on: main (${PULL_BASE_SHA})"
+
+if ! make test; then
+	echo -e "${RED}✗ make test\\n${NC}"
+	exit 1
+else
+    coverage_main=$(go tool cover -func=cover.out | grep total | awk '{print substr($3,1,length($3)-1)}')
+	echo -e "Total coverage on main: ${coverage_main}%"
+	echo -e "${GREEN}√ make test${NC}"
+    rm cover.out
+fi
+
+if awk "BEGIN {exit !($coverage_pr >= $coverage_main)}"; then
+	echo -e "${GREEN}√ Thanks for keeping & increasing code coverage!"
+else
+	echo -e "${RED}✗ This PR lowering code coverage compared to main branch! Please add tests.\\n${NC}"
+	exit 0 # DO NOT FAIL for now, let get team approval first
+fi
