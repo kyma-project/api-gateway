16425204215e4c8cc203eb2cd5a925cb8df778fc
Vladimir Videlov <vladimir.videlov@sap.com>
2023-04-04 12:11:57 +0200

    Typo

--- tests/integration/test_helpers.go
+++ tests/integration/test_helpers.go
@@ -40,7 +40,7 @@ const (
 	cucumberFileName          = "cucumber-report.json"
 	anyToken                  = "any"
 	authorizationHeaderName   = "Authorization"
-	authorizationHeaderPrefix = "Baerer"
+	authorizationHeaderPrefix = "Bearer"
 	opaqueHeaderName          = "opaque-token"
 	defaultNS                 = "kyma-system"
 	configMapName             = "api-gateway-config"
