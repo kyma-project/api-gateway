4636a054bda6fe07b5a65f86372c0c2c20ec2abf
Vladimir Videlov <vladimir.videlov@sap.com>
2023-05-23 09:34:39 +0200

    euo pipefail

--- tests/integration/scripts/integration-gardener-custom-domain.sh
+++ tests/integration/scripts/integration-gardener-custom-domain.sh
@@ -2,7 +2,8 @@
 #
 ##Description: This scripts installs and test api-gateway custom domain test using the CLI on a real Gardener GCP cluster.
 ## exit on error, and raise error when variable is not set when used
-set -e
+
+set -euo pipefail
 
 function check_required_vars() {
   local requiredVarMissing=false
