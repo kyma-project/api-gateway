83f7aa5cba90abd1e208a945f3eba00aaec0f62a
Tim Riffer <tim.riffer@sap.com>
2023-06-26 08:26:33 +0200

    Fix custom domain tests script

--- tests/integration/scripts/custom-domain-gardener-aws.sh
+++ tests/integration/scripts/custom-domain-gardener-aws.sh
@@ -15,4 +15,4 @@ export GARDENER_PROVIDER="aws"
 export GARDENER_REGION="eu-central-1"
 export GARDENER_ZONES="eu-central-1b,eu-central-1c,eu-central-1a"
 
-./tests/integration/scripts/custom-domain-gardner.sh
+./tests/integration/scripts/custom-domain-gardener.sh
