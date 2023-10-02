9462ca6fb5a39ed8c3f616a248685b7304d7d303
Patryk Strugacz <patryk.strugacz@sap.com>
2023-10-02 14:47:41 +0200

    Add gateway disabling test

--- tests/integration/testsuites/gateway/testsuite.go
+++ tests/integration/testsuites/gateway/testsuite.go
@@ -1,13 +1,20 @@
 package gateway
 
 import (
+	"context"
 	_ "embed"
+	"fmt"
 	"github.com/cucumber/godog"
 	"github.com/kyma-project/api-gateway/tests/integration/pkg/helpers"
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/manifestprocessor"
 	"github.com/kyma-project/api-gateway/tests/integration/pkg/resource"
 	"github.com/kyma-project/api-gateway/tests/integration/pkg/testcontext"
 	"golang.org/x/oauth2/clientcredentials"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
+	"log"
+	"time"
 )
 
 type testsuite struct {
@@ -41,10 +48,46 @@ func (t *testsuite) K8sClient() dynamic.Interface {
 }
 
 func (t *testsuite) Setup() error {
+	namespace := fmt.Sprintf("%s-%s", t.name, helpers.GenerateRandomString(6))
+	log.Printf("Using namespace: %s\n", namespace)
+
+	// create common resources for all scenarios
+	globalCommonResources, err := manifestprocessor.ParseFromFileWithTemplate("global-commons.yaml", manifestsPath, struct {
+		Namespace string
+	}{
+		Namespace: namespace,
+	})
+	if err != nil {
+		return err
+	}
+
+	// delete test namespace if the previous test namespace persists
+	nsResourceSchema, ns, name := t.resourceManager.GetResourceSchemaAndNamespace(globalCommonResources[0])
+	log.Printf("Delete test namespace, if exists: %s\n", name)
+	err = t.resourceManager.DeleteResource(t.k8sClient, nsResourceSchema, ns, name)
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(time.Duration(t.config.ReqDelay) * time.Second)
+
+	log.Printf("Creating common tests resources")
+	_, err = t.resourceManager.CreateResources(t.k8sClient, globalCommonResources...)
+	if err != nil {
+		return err
+	}
+
+	t.namespace = namespace
+
 	return nil
 }
 
 func (t *testsuite) TearDown() {
+	res := schema.GroupVersionResource{Group: "", Version: "v1", Resource: "namespaces"}
+	err := t.k8sClient.Resource(res).Delete(context.Background(), t.namespace, v1.DeleteOptions{})
+	if err != nil {
+		log.Print(err.Error())
+	}
 }
 
 func NewTestsuite(httpClient *helpers.RetryableHttpClient, k8sClient dynamic.Interface, rm *resource.Manager, config testcontext.Config) testcontext.Testsuite {
