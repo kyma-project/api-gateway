652989868c0b1020501d3f2e99880719cd18a2d1
Chwila <bartosz.chwila@sap.com>
2023-10-10 15:04:47 +0200

    Readd cancel

--- controllers/operator/suite_test.go
+++ controllers/operator/suite_test.go
@@ -129,6 +129,11 @@ var _ = BeforeSuite(func() {
 })
 
 var _ = AfterSuite(func() {
+	/*
+		 Provided solution for timeout issue waiting for kubeapiserver
+			https://github.com/kubernetes-sigs/controller-runtime/issues/1571#issuecomment-1005575071
+	*/
+	cancel()
 	By("Tearing down the test environment")
 	err := retry.OnError(retry.DefaultBackoff, func(err error) bool {
 		return true
