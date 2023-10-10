8a99f743321d7795426908faa636714356e5c64d
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2023-10-10 10:49:39 +0200

    More integration tests

--- tests/integration/testsuites/gateway/scenario.go
+++ tests/integration/testsuites/gateway/scenario.go
@@ -11,7 +11,6 @@ import (
 	"github.com/kyma-project/api-gateway/tests/integration/pkg/testcontext"
 	v12 "k8s.io/api/apps/v1"
 	v2 "k8s.io/api/autoscaling/v2"
-	"k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -22,47 +21,6 @@ import (
 )
 
 const manifestsPath = "testsuites/gateway/manifests/"
-
-type godogResourceMapping int
-
-func (k godogResourceMapping) String() string {
-	switch k {
-	case Deployment:
-		return "Deployment"
-	case Service:
-		return "Service"
-	case HorizontalPodAutoscaler:
-		return "HorizontalPodAutoscaler"
-	case ConfigMap:
-		return "ConfigMap"
-	case Secret:
-		return "Secret"
-	case CustomResourceDefinition:
-		return "CustomResourceDefinition"
-	case ServiceAccount:
-		return "ServiceAccount"
-	case ClusterRole:
-		return "ClusterRole"
-	case ClusterRoleBinding:
-		return "ClusterRoleBinding"
-	case PeerAuthentication:
-		return "PeerAuthentication"
-	}
-	panic(fmt.Errorf("%#v has unimplemented String() method", k))
-}
-
-const (
-	Deployment godogResourceMapping = iota
-	Service
-	HorizontalPodAutoscaler
-	ConfigMap
-	Secret
-	CustomResourceDefinition
-	ServiceAccount
-	ClusterRole
-	ClusterRoleBinding
-	PeerAuthentication
-)
 
 type scenario struct {
 	testID          string
@@ -80,18 +38,19 @@ func initScenario(ctx *godog.ScenarioContext, ts *testsuite) {
 	}
 
 	ctx.Step(`APIGateway CR is applied`, scenario.applyAPIGatewayCR)
-	ctx.Step(`^APIGateway CR is in "([^"]*)" state$`, scenario.thereIsAnAPIGatewayCR)
-	ctx.Step(`^there is a "([^"]*)" gateway in "([^"]*)" namespace$`, scenario.thereIsAGateway)
+	ctx.Step(`^APIGateway CR "([^"]*)" "([^"]*)" present$`, scenario.thereIsAnAPIGatewayCR)
+	ctx.Step(`^APIGateway CR is in Ready state$`, scenario.APIGatewayCRisReady)
+	ctx.Step(`^there is APIGateway CR "([^"]*)" in "([^"]*)" namespace$`, scenario.thereIsAGateway)
 	ctx.Step(`^there is a "([^"]*)" secret in "([^"]*)" namespace$`, scenario.thereIsACertificate)
 	ctx.Step(`^there is an "([^"]*)" APIRule$`, scenario.thereIsAnAPIRule)
 	ctx.Step(`^APIRule "([^"]*)" is removed$`, scenario.deleteAPIRule)
 	ctx.Step(`^disabling kyma gateway will result in "([^"]*)" state$`, scenario.disableKymaGatewayAndCheckStatus)
-	ctx.Step(`^gateway "([^"]*)" is removed$`, scenario.deleteGateway)
+	ctx.Step(`^APIGateway CR "([^"]*)" is removed$`, scenario.deleteAPIGatewayCR)
 	ctx.Step(`^gateway "([^"]*)" in "([^"]*)" namespace does not exist$`, scenario.thereIsNoGateway)
 	ctx.Step(`^there is a "([^"]*)" Gardener Certificate CR in "([^"]*)" namespace$`, scenario.thereIsACertificateCR)
 	ctx.Step(`^there is a "([^"]*)" Gardener DNSEntry CR in "([^"]*)" namespace$`, scenario.thereIsADNSEntryCR)
-	ctx.Step(`there is a "([^"]*)" "([^"]*)" in the cluster`, scenario.resourceIsPresent)
-	ctx.Step(`there is a "([^"]*)" "([^"]*)" in namespace "([^"]*)"`, scenario.namespacedResourceIsPresent)
+	ctx.Step(`there "([^"]*)" "([^"]*)" "([^"]*)" in the cluster`, scenario.resourceIsPresent)
+	ctx.Step(`there "([^"]*)" "([^"]*)" "([^"]*)" in namespace "([^"]*)"`, scenario.namespacedResourceIsPresent)
 	ctx.Step(`"([^"]*)" "([^"]*)" in namespace "([^"]*)" has status "([^"]*)"`, scenario.namespacedResourceHasStatusReady)
 }
 
@@ -126,13 +85,32 @@ func (c *scenario) applyAPIGatewayCR() error {
 	return nil
 }
 
-func (c *scenario) thereIsAnAPIGatewayCR(state string) error {
+func (c *scenario) thereIsAnAPIGatewayCR(present string) error {
+	const (
+		is   = "is"
+		isNo = "is no"
+	)
+	if present != is && present != isNo {
+		panic(fmt.Errorf("choose %s or %s", is, isNo))
+	}
+	return retry.Do(func() error {
+		res := schema.GroupVersionResource{Group: "operator.kyma-project.io", Version: "v1alpha1", Resource: "apigateways"}
+		_, err := c.k8sClient.Resource(res).Get(context.Background(), c.config.GatewayCRName, v1.GetOptions{})
+		if present == is && err != nil {
+			return fmt.Errorf("apigateway should be present but is not")
+		} else if present != is && err == nil {
+			return fmt.Errorf("apigateway should not be present but is")
+		}
+
+		return nil
+	}, testcontext.GetRetryOpts(c.config)...)
+}
+
+func (c *scenario) APIGatewayCRisReady() error {
+	const state = "Ready"
 	return retry.Do(func() error {
 		res := schema.GroupVersionResource{Group: "operator.kyma-project.io", Version: "v1alpha1", Resource: "apigateways"}
 		gateway, err := c.k8sClient.Resource(res).Get(context.Background(), c.config.GatewayCRName, v1.GetOptions{})
-		if err != nil {
-			return fmt.Errorf("gateway could not be found")
-		}
 
 		gatewayState, found, err := unstructured.NestedString(gateway.Object, "status", "state")
 		if err != nil || !found {
@@ -140,9 +118,8 @@ func (c *scenario) thereIsAnAPIGatewayCR(state string) error {
 		} else if gatewayState != state {
 			return fmt.Errorf("gateway state %s, is not in the expected state %s", gatewayState, state)
 		}
-
 		return nil
-	}, testcontext.GetRetryOpts(c.config)...)
+	})
 }
 
 func (c *scenario) thereIsAGateway(name string, namespace string) error {
@@ -220,7 +197,7 @@ func (c *scenario) deleteAPIRule(name string) error {
 	}, testcontext.GetRetryOpts(c.config)...)
 }
 
-func (c *scenario) deleteGateway(name string) error {
+func (c *scenario) deleteAPIGatewayCR(name string) error {
 	customDomainManifestDirectory := path.Dir(manifestsPath)
 	gateway, err := manifestprocessor.ParseFromFileWithTemplate("kyma-gateway-enabled.yaml", customDomainManifestDirectory, struct {
 		NamePrefix string
@@ -312,43 +289,59 @@ func (c *scenario) thereIsADNSEntryCR(name, namespace string) error {
 	return nil
 }
 
-func (c *scenario) resourceIsPresent(kind, name string) error {
+func (c *scenario) resourceIsPresent(isPresent, kind, name string) error {
+	const (
+		is   = "is"
+		isNo = "is no"
+	)
+	if isPresent != is && isPresent != isNo {
+		panic(fmt.Errorf("choose %s or %s", is, isNo))
+	}
+
 	return retry.Do(func() error {
-		gvr := getResourceGvr(kind, name)
+		gvr := resource.GetResourceGvr(kind, name)
 		_, err := c.k8sClient.Resource(gvr).Get(context.Background(), name, v1.GetOptions{})
-		if err != nil {
-			if errors.IsNotFound(err) {
-				return fmt.Errorf("could not find non namespaced resource %s, named: %s", kind, name)
-			}
-			return err
+		if isPresent == is && err != nil {
+			return fmt.Errorf("apigateway should be present but is not")
+		} else if isPresent != is && err == nil {
+			return fmt.Errorf("apigateway should not be present but is")
 		}
+
 		return nil
 	}, testcontext.GetRetryOpts(c.config)...)
 }
 
-func (c *scenario) namespacedResourceIsPresent(kind, name, namespace string) error {
+func (c *scenario) namespacedResourceIsPresent(isPresent, kind, name, namespace string) error {
+	const (
+		is   = "is"
+		isNo = "is no"
+	)
+	if isPresent != is && isPresent != isNo {
+		panic(fmt.Errorf("choose %s or %s", is, isNo))
+	}
 	return retry.Do(func() error {
-		gvr := getResourceGvr(kind, name)
+		gvr := resource.GetResourceGvr(kind, name)
 		_, err := c.k8sClient.Resource(gvr).Namespace(namespace).Get(context.Background(), name, v1.GetOptions{})
-		if err != nil {
-			if errors.IsNotFound(err) {
-				return fmt.Errorf("could not find resource: %s, named: %s, in namespace %s", kind, name, namespace)
-			}
-			return err
+
+		if isPresent == is && err != nil {
+			return fmt.Errorf("apigateway should be present but is not")
+		} else if isPresent != is && err == nil {
+			return fmt.Errorf("apigateway should not be present but is")
 		}
+
 		return nil
 	}, testcontext.GetRetryOpts(c.config)...)
 }
 
 func (c *scenario) namespacedResourceHasStatusReady(kind, name, namespace string) error {
 	return retry.Do(func() error {
-		gvr := getResourceGvr(kind, name)
+		gvr := resource.GetResourceGvr(kind, name)
 		unstr, err := c.k8sClient.Resource(gvr).Namespace(namespace).Get(context.Background(), name, v1.GetOptions{})
 		if err != nil {
 			return fmt.Errorf("could not get resource: %s, named: %s, in namespace %s", kind, name, namespace)
 		}
 		switch kind {
-		case Deployment.String():
+		case resource.Deployment.String():
 			var dep v12.Deployment
 			err := runtime.DefaultUnstructuredConverter.FromUnstructured(unstr.UnstructuredContent(), &dep)
 			if err != nil {
@@ -357,7 +350,7 @@ func (c *scenario) namespacedResourceHasStatusReady(kind, name, namespace string
 			if dep.Status.UnavailableReplicas != 0 {
 				return fmt.Errorf("kind: %s, name %s, namespace %s, is not Ready", kind, name, namespace)
 			}
-		case HorizontalPodAutoscaler.String():
+		case resource.HorizontalPodAutoscaler.String():
 			var hpa v2.HorizontalPodAutoscaler
 			err := runtime.DefaultUnstructuredConverter.FromUnstructured(unstr.UnstructuredContent(), &hpa)
 			if err != nil {
@@ -371,73 +364,4 @@ func (c *scenario) namespacedResourceHasStatusReady(kind, name, namespace string
 		}
 		return nil
 	}, testcontext.GetRetryOpts(c.config)...)
-}
-
-func getResourceGvr(kind, name string) schema.GroupVersionResource {
-	var gvr schema.GroupVersionResource
-	switch kind {
-	case Deployment.String():
-		gvr = schema.GroupVersionResource{
-			Group:    "apps",
-			Version:  "v1",
-			Resource: "deployments",
-		}
-	case Service.String():
-		gvr = schema.GroupVersionResource{
-			Group:    "",
-			Version:  "v1",
-			Resource: "services",
-		}
-	case HorizontalPodAutoscaler.String():
-		gvr = schema.GroupVersionResource{
-			Group:    "autoscaling",
-			Version:  "v2",
-			Resource: "horizontalpodautoscalers",
-		}
-	case ConfigMap.String():
-		gvr = schema.GroupVersionResource{
-			Group:    "",
-			Version:  "v1",
-			Resource: "configmaps",
-		}
-	case Secret.String():
-		gvr = schema.GroupVersionResource{
-			Group:    "",
-			Version:  "v1",
-			Resource: "secrets",
-		}
-	case CustomResourceDefinition.String():
-		gvr = schema.GroupVersionResource{
-			Group:    "apiextensions.k8s.io",
-			Version:  "v1",
-			Resource: "customresourcedefinitions",
-		}
-	case ServiceAccount.String():
-		gvr = schema.GroupVersionResource{
-			Group:    "",
-			Version:  "v1",
-			Resource: "serviceaccounts",
-		}
-	case ClusterRole.String():
-		gvr = schema.GroupVersionResource{
-			Group:    "rbac.authorization.k8s.io",
-			Version:  "v1",
-			Resource: "clusterroles",
-		}
-	case ClusterRoleBinding.String():
-		gvr = schema.GroupVersionResource{
-			Group:    "rbac.authorization.k8s.io",
-			Version:  "v1",
-			Resource: "clusterrolebindings",
-		}
-	case PeerAuthentication.String():
-		gvr = schema.GroupVersionResource{
-			Group:    "security.istio.io",
-			Version:  "v1beta1",
-			Resource: "peerauthentications",
-		}
-	default:
-		panic(fmt.Errorf("cannot get gvr for kind: %s, name: %s", kind, name))
-	}
-	return gvr
 }
