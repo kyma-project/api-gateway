104ce48be56b69d6676be2f10a675a372ccbeb89
Chwila <bartosz.chwila@sap.com>
2023-10-18 14:17:53 +0200

    Use interface

--- internal/dependencies/dependencies.go
+++ internal/dependencies/dependencies.go
@@ -2,18 +2,17 @@ package dependencies
 
 import (
 	"context"
-	"github.com/kyma-project/api-gateway/controllers"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type Dependencies interface {
-	AreAvailable(context.Context, client.Client) controllers.Status
+	AreAvailable(ctx context.Context, k8sClient client.Client) (string, error)
 }
 
-func ApiGateway() *dependecies {
-	return &dependecies{
+func ApiGateway() Dependencies {
+	return &dependencies{
 		CRDNames: []string{
 			"gateways.networking.istio.io",
 			"virtualservices.networking.istio.io",
@@ -21,8 +20,8 @@ func ApiGateway() *dependecies {
 	}
 }
 
-func GardenerAPIGateway() *dependecies {
-	return &dependecies{
+func GardenerAPIGateway() Dependencies {
+	return &dependencies{
 		CRDNames: []string{
 			"gateways.networking.istio.io",
 			"virtualservices.networking.istio.io",
@@ -32,8 +31,8 @@ func GardenerAPIGateway() *dependecies {
 	}
 }
 
-func APIRule() *dependecies {
-	return &dependecies{
+func APIRule() Dependencies {
+	return &dependencies{
 		CRDNames: []string{
 			"virtualservices.networking.istio.io",
 			"rules.oathkeeper.ory.sh",
@@ -43,12 +42,12 @@ func APIRule() *dependecies {
 	}
 }
 
-type dependecies struct {
+type dependencies struct {
 	CRDNames []string
 }
 
 // AreAvailable checks whether pre-requisite CRDs are present on the cluster. It returns the name of the not found CRD and an error if it is not found.
-func (d dependecies) AreAvailable(ctx context.Context, k8sClient client.Client) (string, error) {
+func (d dependencies) AreAvailable(ctx context.Context, k8sClient client.Client) (string, error) {
 	for _, name := range d.CRDNames {
 		err := k8sClient.Get(ctx, types.NamespacedName{Name: name}, &apiextensionsv1.CustomResourceDefinition{})
 		if err != nil {
