bebfb89e28b54829c9ff2436a8da232cb992d7fb
Vladimir Videlov <vladimir.videlov@sap.com>
2023-10-31 11:31:47 +0100

    Fix order for ORY Oathkeeper resources (clusterrole and binding)

--- internal/reconciliations/oathkeeper/maester/maester.go
+++ internal/reconciliations/oathkeeper/maester/maester.go
@@ -3,6 +3,7 @@ package maester
 import (
 	"context"
 	"errors"
+
 	"github.com/kyma-project/api-gateway/apis/operator/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -11,7 +12,7 @@ func ReconcileMaester(ctx context.Context, k8sClient client.Client, apiGatewayCR
 	return errors.Join(
 		reconcileOryOathkeeperPeerAuthentication(ctx, k8sClient, apiGatewayCR),
 		reconcileOryOathkeeperMaesterServiceAccount(ctx, k8sClient, apiGatewayCR),
-		reconcileOryOathkeeperMaesterClusterRoleBinding(ctx, k8sClient, apiGatewayCR),
 		reconcileOryOathkeeperMaesterClusterRole(ctx, k8sClient, apiGatewayCR),
+		reconcileOryOathkeeperMaesterClusterRoleBinding(ctx, k8sClient, apiGatewayCR),
 	)
 }
