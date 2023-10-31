e93b457d9beeacc5f5cac26c83e076b25ed2d54e
Chwila <bartosz.chwila@sap.com>
2023-10-31 13:33:06 +0100

    Adjust

--- tests/ui/tests/scripts/kyma-dashboard-integration.sh
+++ tests/ui/tests/scripts/kyma-dashboard-integration.sh
@@ -6,7 +6,6 @@ export NO_COLOR=1
 export REPO_IMG_DEV="k3d-registry.localhost:5000/kyma-dashboard"
 export TAG="test-dev"
 
-apt-get update -y 
 apt-get install -y gettext-base
 
 function deploy_k3d_kyma (){
