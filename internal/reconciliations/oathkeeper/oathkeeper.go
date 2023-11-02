3a45a8883dd0faf5b2faca75e761fce2fc20658d
Vladimir Videlov <vladimir.videlov@sap.com>
2023-11-02 12:46:08 +0100

    Reconcile maester at the end

--- internal/reconciliations/oathkeeper/oathkeeper.go
+++ internal/reconciliations/oathkeeper/oathkeeper.go
@@ -3,6 +3,7 @@ package oathkeeper
 import (
 	"context"
 	"errors"
+
 	"github.com/kyma-project/api-gateway/apis/operator/v1alpha1"
 	"github.com/kyma-project/api-gateway/controllers"
 	"github.com/kyma-project/api-gateway/internal/reconciliations/oathkeeper/cronjob"
@@ -13,7 +14,6 @@ import (
 func ReconcileOathkeeper(ctx context.Context, k8sClient client.Client, apiGatewayCR *v1alpha1.APIGateway) controllers.Status {
 	err := errors.Join(
 		reconcileOryOathkeeperRuleCRD(ctx, k8sClient, *apiGatewayCR),
-		maester.ReconcileMaester(ctx, k8sClient, *apiGatewayCR),
 		reconcileOryJWKSSecret(ctx, k8sClient, *apiGatewayCR),
 		reconcileOryOathkeeperConfigConfigMap(ctx, k8sClient, *apiGatewayCR),
 		reconcileOathkeeperHPA(ctx, k8sClient, *apiGatewayCR),
@@ -21,6 +21,7 @@ func ReconcileOathkeeper(ctx context.Context, k8sClient client.Client, apiGatewa
 		reconcileOryOathkeeperServices(ctx, k8sClient, *apiGatewayCR),
 		cronjob.ReconcileCronjob(ctx, k8sClient, *apiGatewayCR),
 		reconcileOathkeeperDeployment(ctx, k8sClient, *apiGatewayCR),
+		maester.ReconcileMaester(ctx, k8sClient, *apiGatewayCR),
 	)
 	if err != nil {
 		return controllers.ErrorStatus(err, "Oathkeeper did not reconcile successfully")
