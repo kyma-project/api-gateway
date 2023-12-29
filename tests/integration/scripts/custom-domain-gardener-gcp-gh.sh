85eb2c3d74231cae0c926106d30771c3e8e994fd
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2023-12-29 14:51:29 +0100

    Revert "change region and machine"
    This reverts commit 3686749a52333d9f6606d864327e1bbef8082e82.

--- tests/integration/scripts/custom-domain-gardener-gcp-gh.sh
+++ tests/integration/scripts/custom-domain-gardener-gcp-gh.sh
@@ -6,14 +6,14 @@
 
 set -e
 
-export MACHINE_TYPE="n1-standard-4"
+export MACHINE_TYPE="n2-standard-4"
 export DISK_SIZE=50
 export DISK_TYPE="pd-standard"
 export SCALER_MAX=3
 export SCALER_MIN=1
 export GARDENER_PROVIDER="gcp"
-export GARDENER_REGION="europe-west4"
-export GARDENER_ZONES="europe-west4-c,europe-west4-b,europe-west4-a"
+export GARDENER_REGION="europe-west3"
+export GARDENER_ZONES="europe-west3-c,europe-west3-b,europe-west3-a"
 export GARDENER_PROVIDER_SECRET_NAME="trial-secretbinding-gcp"
 export GARDENER_PROJECT_NAME="goatz"
 export GARDENER_CLUSTER_VERSION="1.26.9"
