451190a86299969d68f7faaf04c9b3f49ea34a63
Vladimir Videlov <vladimir.videlov@sap.com>
2024-01-23 16:39:54 +0100

    Update comment to current

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 const defaultModuleVersion string = "dev"
 
 var (
-	// dev is the fallback version, as the variable is set to the correct version by the Dockerfile during build time.
+	// variable is set to the correct version by the Dockerfile during build time.
 	version string
 )
 
