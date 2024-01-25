93c9982798585e551b1b7df18bd95340a771e8e9
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2024-01-25 12:47:11 +0100

    Move remaining aws jobs

---
+++ tests/integration/scripts/custom-domain-gardener-aws-gh.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#
+##Description: This scripts installs and tests api-gateway custom domain test as well as gateway test using the CLI on a real Gardener AWS cluster.
+## exit on error, and raise error when variable is not set when used
+
+set -e
+
+export MACHINE_TYPE="m5.xlarge"
+export DISK_SIZE=50
+export DISK_TYPE="gp2"
+export SCALER_MAX=3
+export SCALER_MIN=1
+export GARDENER_PROVIDER="aws"
+export GARDENER_REGION="eu-west-1"
+export GARDENER_ZONES="eu-west-1b,eu-west-1c,eu-west-1a"
+export GARDENER_PROVIDER_SECRET_NAME="aws-gardener-access"
+export GARDENER_PROJECT_NAME="goatz"
+export GARDENER_CLUSTER_VERSION="1.26.11"
+
+./tests/integration/scripts/custom-domain-gardener-gh.sh
