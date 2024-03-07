870202926ecb7cc16668f69aeec71962e75ba401
Tim Riffer <tim.riffer@sap.com>
2024-03-07 11:31:34 +0100

    Revert "Install chromium and run tests"
    This reverts commit 78db11b68ca37e81637958cb754b4fef4dae64d9.

--- tests/ui/tests/plugins/index.js
+++ tests/ui/tests/plugins/index.js
@@ -1,52 +1,18 @@
 const fs = require('fs');
-const os = require('os');
-const puppeteer = require('@puppeteer/browsers');
 
-const chromeVersion = "121.0.6167.85"
-
-module.exports = async (on, config) => {
-    on('after:spec', (spec, results) => {
-        if (results && results.video) {
-            // Do we have failures for any retry attempts?
-            const failures = results.tests.some((test) =>
-                test.attempts.some((attempt) => attempt.state === 'failed')
-            )
-            if (!failures) {
-                // delete the video if the spec passed and no tests retried
-                fs.unlinkSync(results.video)
-            }
-        }
-    });
-
-    if (isLinux()) {
-        const chromium = await installChromium()
-        config.browsers.push(chromium)
+module.exports = (on, config) => {
+  on('after:spec', (spec, results) => {
+    if (results && results.video) {
+      // Do we have failures for any retry attempts?
+      const failures = results.tests.some((test) =>
+          test.attempts.some((attempt) => attempt.state === 'failed')
+      )
+      if (!failures) {
+        // delete the video if the spec passed and no tests retried
+        fs.unlinkSync(results.video)
+      }
     }
+  });
 
-    return config;
+  return config;
 };
-
-function isLinux() {
-    return os.platform() === 'linux'
-}
-
-async function installChromium() {
-    process.cwd()
-
-    const browser = await puppeteer.install({
-        cacheDir: process.cwd() + "/cypress/browsers/chromium",
-        browser: "chrome",
-        buildId: chromeVersion,
-        baseUrl: "https://storage.googleapis.com/chrome-for-testing-public",
-    })
-
-    return {
-        name: 'chromium',
-        family: 'chromium',
-        displayName: 'Chromium',
-        version: browser.buildId,
-        majorVersion: browser.buildId,
-        path: browser.executablePath,
-        channel: 'ci'
-    }
-}
