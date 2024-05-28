413ee88a7bd3cf21e380c9c96ce78b6996998b30
Patryk Strugacz <werdes72@users.noreply.github.com>
2024-05-28 13:05:36 +0200

    Run tests on gardener live (#1087)
    * Run tests on gardener live
    
    * Temporarily run gardener tests on PR
    
    * Fix wrong token
    
    * Use gardener live env
    
    * Revert
    
    * Revert obsolete changes

--- tests/integration/scripts/custom-domain-gardener-aws-gh.sh
+++ tests/integration/scripts/custom-domain-gardener-aws-gh.sh
@@ -14,7 +14,7 @@ export SCALER_MIN=1
 export GARDENER_PROVIDER="aws"
 export GARDENER_REGION="eu-west-1"
 export GARDENER_PROVIDER_SECRET_NAME="aws-gardener-access"
-export GARDENER_PROJECT_NAME="goatz"
+export GARDENER_PROJECT_NAME="goats"
 export GARDENER_CLUSTER_VERSION="1.27.8"
 
 ./tests/integration/scripts/custom-domain-gardener-gh.sh
--- tests/integration/scripts/custom-domain-gardener-gcp-gh.sh
+++ tests/integration/scripts/custom-domain-gardener-gcp-gh.sh
@@ -14,7 +14,7 @@ export SCALER_MIN=1
 export GARDENER_PROVIDER="gcp"
 export GARDENER_REGION="europe-west3"
 export GARDENER_PROVIDER_SECRET_NAME="goat"
-export GARDENER_PROJECT_NAME="goatz"
+export GARDENER_PROJECT_NAME="goats"
 export GARDENER_CLUSTER_VERSION="1.27.8"
 
 ./tests/integration/scripts/custom-domain-gardener-gh.sh
