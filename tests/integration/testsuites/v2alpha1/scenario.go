0f4740761e6420f49765af31d78c5771abc2b341
Tim Riffer <tim.riffer@sap.com>
2024-07-15 14:45:39 +0200

    Add first v2alpha1 integration test feature

---
+++ tests/integration/testsuites/v2alpha1/scenario.go
@@ -0,0 +1,177 @@
+package v2alpha1
+
+import (
+	_ "embed"
+	"fmt"
+	"github.com/avast/retry-go/v4"
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/auth"
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/helpers"
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/manifestprocessor"
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/resource"
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/testcontext"
+	"golang.org/x/oauth2/clientcredentials"
+	"k8s.io/client-go/dynamic"
+	"net/http"
+	"strings"
+)
+
+type scenario struct {
+	Namespace               string
+	TestID                  string
+	Domain                  string
+	ApiResourceManifestPath string
+	ApiResourceDirectory    string
+	ManifestTemplate        map[string]string
+	Url                     string
+	k8sClient               dynamic.Interface
+	oauth2Cfg               *clientcredentials.Config
+	jwtConfig               *clientcredentials.Config
+	httpClient              *helpers.RetryableHttpClient
+	resourceManager         *resource.Manager
+	config                  testcontext.Config
+}
+
+func (s *scenario) callingTheEndpointWithValidTokenShouldResultInStatusBetween(path string, tokenType string, lower, higher int) error {
+	asserter := &helpers.StatusPredicate{LowerStatusBound: lower, UpperStatusBound: higher}
+	return s.callingTheEndpointWithMethodWithValidToken(fmt.Sprintf("%s%s", s.Url, path), http.MethodGet, tokenType, asserter)
+}
+
+func (s *scenario) callingTheEndpointWithMethodWithValidTokenShouldResultInStatusBetween(path string, method string, tokenType string, lower, higher int) error {
+	asserter := &helpers.StatusPredicate{LowerStatusBound: lower, UpperStatusBound: higher}
+	return s.callingTheEndpointWithMethodWithValidToken(fmt.Sprintf("%s%s", s.Url, path), method, tokenType, asserter)
+}
+
+func (s *scenario) callingTheEndpointWithMethodWithValidToken(url string, method string, tokenType string, asserter helpers.HttpResponseAsserter) error {
+
+	requestHeaders := make(map[string]string)
+
+	switch tokenType {
+	case "JWT":
+		tokenJwt, err := auth.GetAccessToken(*s.jwtConfig, "jwt")
+		if err != nil {
+			return fmt.Errorf("failed to fetch an id_token: %s", err.Error())
+		}
+
+		requestHeaders[testcontext.AuthorizationHeaderName] = fmt.Sprintf("Bearer %s", tokenJwt)
+	default:
+		return fmt.Errorf("unsupported token type: %s", tokenType)
+	}
+
+	return s.httpClient.CallEndpointWithHeadersAndMethod(requestHeaders, url, method, asserter)
+}
+
+func (s *scenario) thereIsAHttpbinServiceAndApiRuleIsApplied() error {
+	err := s.thereIsAHttpbinService()
+	if err != nil {
+		return err
+	}
+
+	r, err := manifestprocessor.ParseFromFileWithTemplate(s.ApiResourceManifestPath, s.ApiResourceDirectory, s.ManifestTemplate)
+	if err != nil {
+		return err
+	}
+	return helpers.ApplyApiRule(s.resourceManager.CreateResources, s.resourceManager.UpdateResources, s.k8sClient, testcontext.GetRetryOpts(), r)
+}
+
+func (s *scenario) theAPIRuleIsApplied() error {
+	r, err := manifestprocessor.ParseFromFileWithTemplate(s.ApiResourceManifestPath, s.ApiResourceDirectory, s.ManifestTemplate)
+	if err != nil {
+		return err
+	}
+	return helpers.ApplyApiRule(s.resourceManager.CreateResources, s.resourceManager.UpdateResources, s.k8sClient, testcontext.GetRetryOpts(), r)
+}
+
+func (s *scenario) theAPIRuleIsUpdated(manifest string) error {
+	resourceManifest, err := manifestprocessor.ParseFromFileWithTemplate(manifest, s.ApiResourceDirectory, s.ManifestTemplate)
+	if err != nil {
+		return err
+	}
+	return helpers.UpdateApiRule(s.resourceManager, s.k8sClient, testcontext.GetRetryOpts(), resourceManifest)
+}
+
+func (s *scenario) callingTheEndpointWithMethodWithInvalidTokenShouldResultInStatusBetween(path string, method string, lower, higher int) error {
+	requestHeaders := map[string]string{testcontext.AuthorizationHeaderName: testcontext.AnyToken}
+	return s.httpClient.CallEndpointWithHeadersAndMethod(requestHeaders, fmt.Sprintf("%s%s", s.Url, path), method, &helpers.StatusPredicate{LowerStatusBound: lower, UpperStatusBound: higher})
+}
+
+func (s *scenario) callingTheEndpointWithInvalidTokenShouldResultInStatusBetween(path string, lower, higher int) error {
+	requestHeaders := map[string]string{testcontext.AuthorizationHeaderName: testcontext.AnyToken}
+	return s.httpClient.CallEndpointWithHeadersWithRetries(requestHeaders, fmt.Sprintf("%s%s", s.Url, path), &helpers.StatusPredicate{LowerStatusBound: lower, UpperStatusBound: higher})
+}
+
+func (s *scenario) callingTheEndpointWithoutTokenShouldResultInStatusBetween(path string, lower, higher int) error {
+	return s.httpClient.CallEndpointWithRetries(fmt.Sprintf("%s/%s", s.Url, strings.TrimLeft(path, "/")), &helpers.StatusPredicate{LowerStatusBound: lower, UpperStatusBound: higher})
+}
+
+func (s *scenario) thereIsAHttpbinService() error {
+	resources, err := manifestprocessor.ParseFromFileWithTemplate("testing-app.yaml", s.ApiResourceDirectory, s.ManifestTemplate)
+	if err != nil {
+		return err
+	}
+	_, err = s.resourceManager.CreateResources(s.k8sClient, resources...)
+	if err != nil {
+		return err
+	}
+
+	s.Url = fmt.Sprintf("https://httpbin-%s.%s", s.TestID, s.Domain)
+
+	return nil
+}
+
+// teardownHttpbinService deletes the httpbin service and reset the url in the scenario. This should be considered a temporary solution
+// to reduce resource consumption until we implement a better way to clean up the resources by a scenario. If the test fails before this step the teardown won't be executed.
+func (s *scenario) teardownHttpbinService() error {
+	resources, err := manifestprocessor.ParseFromFileWithTemplate("testing-app.yaml", s.ApiResourceDirectory, s.ManifestTemplate)
+	if err != nil {
+		return err
+	}
+	err = s.resourceManager.DeleteResources(s.k8sClient, resources...)
+	if err != nil {
+		return err
+	}
+
+	s.Url = ""
+
+	return nil
+}
+
+func (s *scenario) preflightEndpointCallResponseHeaders(endpoint, origin string, statusCode int, headerKey, headerValue string) error {
+	headers := map[string]string{
+		"Origin":                        origin,
+		"Access-Control-Request-Method": "GET,POST,PUT,DELETE,PATCH",
+	}
+	return retry.Do(func() error {
+		resp, err := s.httpClient.CallEndpointWithRetriesAndGetResponse(headers, nil, http.MethodOptions, s.Url+endpoint)
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode != statusCode {
+			return fmt.Errorf("expected response status code %d got %d", statusCode, resp.StatusCode)
+		}
+		rhv := resp.Header.Get(headerKey)
+		if rhv != headerValue {
+			return fmt.Errorf("expected header %s with value %s, got %s", headerKey, headerValue, rhv)
+		}
+		return nil
+	}, testcontext.GetRetryOpts()...)
+}
+
+func (s *scenario) preflightEndpointCallNoResponseHeader(endpoint, origin string, statusCode int, headerKey string) error {
+	headers := map[string]string{
+		"Origin":                        origin,
+		"Access-Control-Request-Method": "GET,POST,PUT,DELETE,PATCH",
+	}
+	return retry.Do(func() error {
+		resp, err := s.httpClient.CallEndpointWithRetriesAndGetResponse(headers, nil, http.MethodOptions, s.Url+endpoint)
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode != statusCode {
+			return fmt.Errorf("expected response status code %d got %d", statusCode, resp.StatusCode)
+		}
+		if len(resp.Header.Values(headerKey)) > 0 {
+			return fmt.Errorf("expected that the response will not contain %s header, but did", headerKey)
+		}
+		return nil
+	}, testcontext.GetRetryOpts()...)
+}
---
+++ tests/integration/testsuites/v2alpha1/testsuite.go
@@ -0,0 +1,184 @@
+package v2alpha1
+
+import (
+	"context"
+	_ "embed"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"path"
+	"time"
+
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/hooks"
+
+	"github.com/cucumber/godog"
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/auth"
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/helpers"
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/manifestprocessor"
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/resource"
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/testcontext"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+)
+
+const manifestsDirectory = "testsuites/v2alpha1/manifests/"
+
+func (t *testsuite) createScenario(templateFileName string, scenarioName string) *scenario {
+	ns := t.namespace
+	testId := helpers.GenerateRandomTestId()
+
+	template := make(map[string]string)
+	template["Namespace"] = ns
+	template["NamePrefix"] = scenarioName
+	template["TestID"] = testId
+	template["Domain"] = t.config.Domain
+	template["GatewayName"] = t.config.GatewayName
+	template["GatewayNamespace"] = t.config.GatewayNamespace
+	template["IssuerUrl"] = t.config.IssuerUrl
+	template["EncodedCredentials"] = base64.RawStdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", t.config.ClientID, t.config.ClientSecret)))
+
+	return &scenario{
+		Namespace:               ns,
+		TestID:                  testId,
+		Domain:                  t.config.Domain,
+		ManifestTemplate:        template,
+		ApiResourceManifestPath: templateFileName,
+		ApiResourceDirectory:    path.Dir(manifestsDirectory),
+		k8sClient:               t.K8sClient(),
+		oauth2Cfg:               t.oauth2Cfg,
+		httpClient:              t.httpClient,
+		resourceManager:         t.ResourceManager(),
+		config:                  t.config,
+		jwtConfig:               t.jwtConfig,
+	}
+}
+
+type testsuite struct {
+	name            string
+	namespace       string
+	httpClient      *helpers.RetryableHttpClient
+	k8sClient       dynamic.Interface
+	resourceManager *resource.Manager
+	config          testcontext.Config
+	oauth2Cfg       *clientcredentials.Config
+	jwtConfig       *clientcredentials.Config
+}
+
+func (t *testsuite) InitScenarios(ctx *godog.ScenarioContext) {
+	initExposeMethodsOnPathsNoAuthHandler(ctx, t)
+	initExposeMethodsOnPathsJwtHandler(ctx, t)
+}
+
+func (t *testsuite) FeaturePath() []string {
+	return []string{"testsuites/v2alpha1/features/"}
+}
+
+func (t *testsuite) Name() string {
+	return t.name
+}
+
+func (t *testsuite) ResourceManager() *resource.Manager {
+	return t.resourceManager
+}
+
+func (t *testsuite) K8sClient() dynamic.Interface {
+	return t.k8sClient
+}
+
+func (t *testsuite) Setup() error {
+	namespace := fmt.Sprintf("%s-%s", t.name, helpers.GenerateRandomString(6))
+	log.Printf("Using namespace: %s\n", namespace)
+
+	// create common resources for all scenarios
+	globalCommonResources, err := manifestprocessor.ParseFromFileWithTemplate("global-commons.yaml", manifestsDirectory, struct {
+		Namespace string
+	}{
+		Namespace: namespace,
+	})
+	if err != nil {
+		return err
+	}
+
+	// delete test namespace if the previous test namespace persists
+	nsResourceSchema, ns, name := t.resourceManager.GetResourceSchemaAndNamespace(globalCommonResources[0])
+	log.Printf("Delete test namespace, if exists: %s\n", name)
+	err = t.resourceManager.DeleteResource(t.k8sClient, nsResourceSchema, ns, name)
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(time.Duration(t.config.ReqDelay) * time.Second)
+
+	log.Printf("Creating common tests resources")
+	_, err = t.resourceManager.CreateResources(t.k8sClient, globalCommonResources...)
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(time.Duration(t.config.ReqDelay) * time.Second)
+
+	var tokenURL string
+	if t.config.OIDCConfigUrl == "empty" {
+		issuerUrl, err := auth.ApplyOAuth2MockServer(t.resourceManager, t.k8sClient, namespace, t.config.Domain)
+		if err != nil {
+			return err
+		}
+		t.config.IssuerUrl = fmt.Sprintf("http://mock-oauth2-server.%s.svc.cluster.local", namespace)
+		tokenURL = fmt.Sprintf("%s/oauth2/token", issuerUrl)
+	} else {
+		oidcConfiguration, err := helpers.GetOIDCConfiguration(t.config.OIDCConfigUrl)
+		if err != nil {
+			return err
+		}
+		t.config.IssuerUrl = oidcConfiguration.Issuer
+		tokenURL = oidcConfiguration.TokenEndpoint
+	}
+
+	t.oauth2Cfg = &clientcredentials.Config{
+		ClientID:     t.config.ClientID,
+		ClientSecret: t.config.ClientSecret,
+		TokenURL:     tokenURL,
+		AuthStyle:    oauth2.AuthStyleInHeader,
+		Scopes:       []string{"read"},
+	}
+
+	t.namespace = namespace
+
+	t.jwtConfig = &clientcredentials.Config{
+		ClientID:     t.config.ClientID,
+		ClientSecret: t.config.ClientSecret,
+		TokenURL:     tokenURL,
+		AuthStyle:    oauth2.AuthStyleInHeader,
+	}
+
+	return nil
+}
+
+func (t *testsuite) TearDown() {
+	res := schema.GroupVersionResource{Group: "", Version: "v1", Resource: "namespaces"}
+	err := t.k8sClient.Resource(res).Delete(context.Background(), t.namespace, v1.DeleteOptions{})
+	if err != nil {
+		log.Print(err.Error())
+	}
+}
+
+func (t *testsuite) BeforeSuiteHooks() []func() error {
+	return []func() error{hooks.ApplyAndVerifyApiGatewayCrSuiteHook}
+}
+
+func (t *testsuite) AfterSuiteHooks() []func() error {
+	return []func() error{hooks.DeleteBlockingResourcesSuiteHook, hooks.ApiGatewayCrTearDownSuiteHook}
+}
+
+func NewTestsuite(httpClient *helpers.RetryableHttpClient, k8sClient dynamic.Interface, rm *resource.Manager, config testcontext.Config) testcontext.Testsuite {
+	return &testsuite{
+		name:            "v2alpha1",
+		httpClient:      httpClient,
+		k8sClient:       k8sClient,
+		resourceManager: rm,
+		config:          config,
+	}
+}
