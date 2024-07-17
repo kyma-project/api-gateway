b054fa375e2f476d9bfeefcd373d4dcd5afec543
Chwila <bartosz.chwila@sap.com>
2024-07-17 14:10:52 +0200

    Add annotation test

--- internal/processing/processors/migration/annotation_test.go
+++ internal/processing/processors/migration/annotation_test.go
@@ -1,29 +1,67 @@
-package migration
+package migration_test
 
 import (
+	"context"
 	gatewayv2alpha1 "github.com/kyma-project/api-gateway/apis/gateway/v2alpha1"
+	"github.com/kyma-project/api-gateway/internal/processing"
+	"github.com/kyma-project/api-gateway/internal/processing/processors/migration"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("AnnotationProcessor", func() {
-	DescribeTable("should return correct next step", func(annotation string, expectedStep string) {
+	It("should add migration step annotation", func() {
+		apirule := gatewayv2alpha1.APIRule{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{},
+			},
+		}
+		config := processing.ReconciliationConfig{}
+		scheme := runtime.NewScheme()
+		err := gatewayv2alpha1.AddToScheme(scheme)
+		Expect(err).To(BeNil())
+
+		k8sClient := fake.NewClientBuilder().WithScheme(scheme).Build()
+
 		// when
-		step := nextMigrationStep(&gatewayv2alpha1.APIRule{
+		result, err := migration.NewAnnotationProcessor(config, &apirule).EvaluateReconciliation(context.Background(), k8sClient)
+
+		// then
+		Expect(err).To(BeNil())
+
+		Expect(result).To(HaveLen(1))
+		Expect(result[0].Action.String()).To(Equal("update"))
+		Expect(result[0].Obj.GetAnnotations()).To(HaveKey("gateway.kyma-project.io/migration-step"))
+		Expect(result[0].Obj.GetAnnotations()["gateway.kyma-project.io/migration-step"]).To(Equal("apply-istio-authorization"))
+	})
+
+	It("should remove migration step annotation", func() {
+		apirule := gatewayv2alpha1.APIRule{
 			ObjectMeta: metav1.ObjectMeta{
 				Annotations: map[string]string{
-					annotationName: annotation,
+					"gateway.kyma-project.io/migration-step": "vs-switch-to-service",
 				},
 			},
-		})
+		}
+		config := processing.ReconciliationConfig{}
+		scheme := runtime.NewScheme()
+		err := gatewayv2alpha1.AddToScheme(scheme)
+		Expect(err).To(BeNil())
+
+		k8sClient := fake.NewClientBuilder().WithScheme(scheme).Build()
+
+		// when
+		result, err := migration.NewAnnotationProcessor(config, &apirule).EvaluateReconciliation(context.Background(), k8sClient)
+
 		// then
-		Expect(string(step)).To(Equal(expectedStep))
-	},
-		Entry("should return applyIstioAuthorizationMigrationStep when annotation is not set", "", "apply-istio-authorization"),
-		Entry("should return switchVsToService when current step is applyIstioAuthorizationMigrationStep", "apply-istio-authorization", "vs-switch-to-service"),
-		Entry("should return removeOryRule when current step is switchVsToService", "vs-switch-to-service", "remove-ory-rule"),
-		Entry("should return finished when current step is removeOryRule", "remove-ory-rule", "migration-finished"),
-	)
+		Expect(err).To(BeNil())
+
+		Expect(result).To(HaveLen(1))
+		Expect(result[0].Action.String()).To(Equal("update"))
+		Expect(result[0].Obj.GetAnnotations()).ToNot(HaveKey("gateway.kyma-project.io/migration-step"))
+	})
 })
--- internal/processing/processors/migration/processors_test.go
+++ internal/processing/processors/migration/processors_test.go
@@ -77,3 +77,23 @@ var _ = Describe("NewMigrationProcessors", func() {
 		),
 	)
 })
+
+var _ = Describe("nextMigrationStep", func() {
+	DescribeTable("should return correct next step", func(annotation string, expectedStep string) {
+		// when
+		step := nextMigrationStep(&gatewayv2alpha1.APIRule{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{
+					annotationName: annotation,
+				},
+			},
+		})
+		// then
+		Expect(string(step)).To(Equal(expectedStep))
+	},
+		Entry("should return applyIstioAuthorizationMigrationStep when annotation is not set", "", "apply-istio-authorization"),
+		Entry("should return switchVsToService when current step is applyIstioAuthorizationMigrationStep", "apply-istio-authorization", "vs-switch-to-service"),
+		Entry("should return removeOryRule when current step is switchVsToService", "vs-switch-to-service", "remove-ory-rule"),
+		Entry("should return finished when current step is removeOryRule", "remove-ory-rule", "migration-finished"),
+	)
+})
