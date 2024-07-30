6da2c28e4362636d0414d7f4b102c908f621459f
Chwila <bartosz.chwila@sap.com>
2024-07-30 14:05:24 +0200

    Change name

--- tests/integration/testsuites/v2alpha1/features/request.feature
+++ tests/integration/testsuites/v2alpha1/features/request.feature
@@ -1,16 +1,16 @@
-Feature: Support for request mutators
+Feature: Support for request headers and cookies
 
-  Scenario: Exposing an endpoint with header mutator configured
-    Given MutatorHeader: There is a httpbin service
-    And MutatorHeader: There is an endpoint on path "/headers" with a header mutator setting "x-mutators-test" header to "a-header-value"
-    When MutatorHeader: The APIRule is applied
-    Then MutatorHeader: Calling the "/headers" endpoint should return response with header "X-Mutators-Test" with value "a-header-value"
-    Then MutatorHeader: Teardown httpbin service
+  Scenario: Exposing an endpoint with request header configured
+    Given RequestHeader: There is a httpbin service
+    And RequestHeader: There is an endpoint on path "/headers" with a header mutator setting "x-mutators-test" header to "a-header-value"
+    When RequestHeader: The APIRule is applied
+    Then RequestHeader: Calling the "/headers" endpoint should return response with header "X-Mutators-Test" with value "a-header-value"
+    Then RequestHeader: Teardown httpbin service
 
-  Scenario: Exposing an endpoint with cookie mutator configured
-    Given MutatorCookie: There is a httpbin service
-    And MutatorCookie: There is an endpoint on path "/cookies" with a cookie mutator setting "x-mutators-test" cookie to "a-cookie-value"
-    When MutatorCookie: The APIRule is applied
-    Then MutatorCookie: Calling the "/cookies" endpoint should return response with cookie "x-mutators-test" with value "a-cookie-value"
-    Then MutatorCookie: Teardown httpbin service
+  Scenario: Exposing an endpoint with request cookie configured
+    Given RequestCookie: There is a httpbin service
+    And RequestCookie: There is an endpoint on path "/cookies" with a cookie mutator setting "x-mutators-test" cookie to "a-cookie-value"
+    When RequestCookie: The APIRule is applied
+    Then RequestCookie: Calling the "/cookies" endpoint should return response with cookie "x-mutators-test" with value "a-cookie-value"
+    Then RequestCookie: Teardown httpbin service
 
--- tests/integration/testsuites/v2alpha1/scenario_request_modification.go
+++ tests/integration/testsuites/v2alpha1/scenario_request_modification.go
@@ -7,21 +7,21 @@ import (
 func initRequestHeaders(ctx *godog.ScenarioContext, ts *testsuite) {
 	scenario := ts.createScenario("jwt-request-header.yaml", "jwt-request-header")
 
-	ctx.Step(`MutatorHeader: There is a httpbin service$`, scenario.thereIsAHttpbinService)
-	ctx.Step(`MutatorHeader: There is an endpoint on path "([^"]*)" with a header mutator setting "([^"]*)" header to "([^"]*)"$`, scenario.thereIsAnEndpointWithHeader)
-	ctx.Step(`MutatorHeader: The APIRule is applied$`, scenario.theAPIRuleIsApplied)
-	ctx.Step(`MutatorHeader: Calling the "([^"]*)" endpoint should return response with header "([^"]*)" with value "([^"]*)"$`, scenario.callinTheEndpointShouldResultInBodyContaining)
-	ctx.Step(`MutatorHeader: Teardown httpbin service$`, scenario.teardownHttpbinService)
+	ctx.Step(`RequestHeader: There is a httpbin service$`, scenario.thereIsAHttpbinService)
+	ctx.Step(`RequestHeader: There is an endpoint on path "([^"]*)" with a header mutator setting "([^"]*)" header to "([^"]*)"$`, scenario.thereIsAnEndpointWithHeader)
+	ctx.Step(`RequestHeader: The APIRule is applied$`, scenario.theAPIRuleIsApplied)
+	ctx.Step(`RequestHeader: Calling the "([^"]*)" endpoint should return response with header "([^"]*)" with value "([^"]*)"$`, scenario.callinTheEndpointShouldResultInBodyContaining)
+	ctx.Step(`RequestHeader: Teardown httpbin service$`, scenario.teardownHttpbinService)
 }
 
 func initRequestCookies(ctx *godog.ScenarioContext, ts *testsuite) {
 	scenario := ts.createScenario("jwt-request-cookie.yaml", "jwt-request-cookie")
 
-	ctx.Step(`MutatorCookie: There is a httpbin service$`, scenario.thereIsAHttpbinService)
-	ctx.Step(`MutatorCookie: There is an endpoint on path "([^"]*)" with a cookie mutator setting "([^"]*)" cookie to "([^"]*)"$`, scenario.thereIsAnEndpointWithCookie)
-	ctx.Step(`MutatorCookie: The APIRule is applied$`, scenario.theAPIRuleIsApplied)
-	ctx.Step(`MutatorCookie: Calling the "([^"]*)" endpoint should return response with cookie "([^"]*)" with value "([^"]*)"$`, scenario.callinTheEndpointShouldResultInBodyContaining)
-	ctx.Step(`MutatorCookie: Teardown httpbin service$`, scenario.teardownHttpbinService)
+	ctx.Step(`RequestCookie: There is a httpbin service$`, scenario.thereIsAHttpbinService)
+	ctx.Step(`RequestCookie: There is an endpoint on path "([^"]*)" with a cookie mutator setting "([^"]*)" cookie to "([^"]*)"$`, scenario.thereIsAnEndpointWithCookie)
+	ctx.Step(`RequestCookie: The APIRule is applied$`, scenario.theAPIRuleIsApplied)
+	ctx.Step(`RequestCookie: Calling the "([^"]*)" endpoint should return response with cookie "([^"]*)" with value "([^"]*)"$`, scenario.callinTheEndpointShouldResultInBodyContaining)
+	ctx.Step(`RequestCookie: Teardown httpbin service$`, scenario.teardownHttpbinService)
 }
 
 func (s *scenario) thereIsAnEndpointWithHeader(_, header, headerValue string) error {
