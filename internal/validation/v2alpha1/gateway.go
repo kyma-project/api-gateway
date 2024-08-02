3fe8f4b104e8ca96c45127dc03f96da3cf474856
Patryk Strugacz <patryk.strugacz@sap.com>
2024-08-02 07:48:26 +0200

    Check if gateway exists, add tests

--- internal/validation/v2alpha1/gateway.go
+++ internal/validation/v2alpha1/gateway.go
@@ -1,6 +1,7 @@
 package v2alpha1
 
 import (
+	"fmt"
 	gatewayv2alpha1 "github.com/kyma-project/api-gateway/apis/gateway/v2alpha1"
 	"github.com/kyma-project/api-gateway/internal/validation"
 	networkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
@@ -8,6 +9,24 @@ import (
 
 func validateGateway(parentAttributePath string, gwList networkingv1beta1.GatewayList, apiRule *gatewayv2alpha1.APIRule) []validation.Failure {
 	var failures []validation.Failure
+	gatewayName := *apiRule.Spec.Gateway
+
+	if !gatewayExists(gwList, gatewayName) {
+		failures = append(failures, validation.Failure{
+			AttributePath: parentAttributePath + ".gateway",
+			Message:       "Gateway not found",
+		})
+	}
 
 	return failures
+}
+
+func gatewayExists(gwList networkingv1beta1.GatewayList, gatewayName string) bool {
+	for _, gw := range gwList.Items {
+		if fmt.Sprintf("%s/%s", gw.Namespace, gw.Name) == gatewayName {
+			return true
+		}
+	}
+
+	return false
 }
