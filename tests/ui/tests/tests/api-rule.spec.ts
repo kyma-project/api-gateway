985f2f0676df026f448433f229d97f6491f9d035
Vladimir Videlov <vladimir.videlov@sap.com>
2024-08-02 15:27:42 +0200

    Workaround

--- tests/ui/tests/tests/api-rule.spec.ts
+++ tests/ui/tests/tests/api-rule.spec.ts
@@ -259,12 +259,13 @@ context("API Rule", () => {
 
         cy.inputPairClearAndType('[aria-label="expand Gateway"]', "kyma-system", "kyma-gateway");
 
-        cy.contains('Gateway must exist, specify both Namespace and Name').should('not.exist')
-
         cy.clickSaveButton();
         cy.clickViewTab();
 
         cy.contains('Gateway must be in the format \'{NAMESPACE}/{NAME}\'').should('not.exist')
+
+        cy.clickEditTab();
+        cy.contains('Gateway must exist, specify both Namespace and Name').should('not.exist')
     });
 
     context("Host", () => {
