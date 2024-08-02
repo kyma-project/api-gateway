9d7e966439ceafb56ab726e9c0398bf993413342
Vladimir Videlov <vladimir.videlov@sap.com>
2024-08-02 16:38:37 +0200

    again

--- tests/ui/tests/support/input.ts
+++ tests/ui/tests/support/input.ts
@@ -1,26 +1,10 @@
 Cypress.Commands.add('inputClearAndType', (selector: string, newValue: string): void => {
     cy.get(selector)
         .find('input')
+        .as('inpt')
         .scrollIntoView()
         .click({ force: true })
+    cy.get('@inpt')
         .clear({ force: true })
         .type(newValue, { force: true });
-});
-
-Cypress.Commands.add('inputPairClearAndType', (selector: string, newValueFirst: string, newValueLast: string): void => {
-    cy.get(selector)
-        .find('input')
-        .first()
-        .scrollIntoView()
-        .click({ force: true })
-        .clear({ force: true })
-        .type(newValueFirst, { force: true });
-
-    cy.get(selector)
-        .find('input')
-        .last()
-        .scrollIntoView()
-        .click({ force: true })
-        .clear({ force: true })
-        .type(newValueLast, { force: true });
 });
