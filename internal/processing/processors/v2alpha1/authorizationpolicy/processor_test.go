7b7f8ae56a7b0b9a97f20d24415f247ca0ee89c2
Tim Riffer <tim.riffer@sap.com>
2024-08-02 11:52:44 +0200

    Restructure tests

--- internal/processing/processors/v2alpha1/authorizationpolicy/processor_test.go
+++ internal/processing/processors/v2alpha1/authorizationpolicy/processor_test.go
@@ -10,19 +10,16 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gstruct"
-	"golang.org/x/exp/slices"
 	"istio.io/api/security/v1beta1"
 	securityv1beta1 "istio.io/client-go/pkg/apis/security/v1beta1"
 )
 
-var _ = Describe("Processor", func() {
+var _ = Describe("Processing", func() {
 	serviceName := "example-service"
-	requiredScopeA := "scope-a"
-	requiredScopeB := "scope-b"
 
 	It("should set path to `/*` when the Rule path is `/.*`", func() {
 		// given
-		ruleJwt := newJwtRuleBuilderWithDummyData().
+		ruleJwt := newNoAuthRuleBuilderWithDummyData().
 			withPath("/.*").
 			build()
 
@@ -47,7 +44,7 @@ var _ = Describe("Processor", func() {
 
 	It("should produce one AP for a Rule without service, but service definition on ApiRule level", func() {
 		// given
-		ruleJwt := newJwtRuleBuilderWithDummyData().
+		ruleJwt := newNoAuthRuleBuilderWithDummyData().
 			withPath("/headers").
 			build()
 
@@ -82,7 +79,7 @@ var _ = Describe("Processor", func() {
 			addMethods(http.MethodGet).
 			withServiceName(ruleServiceName).
 			withServicePort(8080).
-			addJwtAuthentication("https://oauth2.example.com/", "https://oauth2.example.com/.well-known/jwks.json").
+			withNoAuth().
 			build()
 
 		apiRule := newAPIRuleBuilderWithDummyData().
@@ -116,7 +113,7 @@ var _ = Describe("Processor", func() {
 		ruleServiceName := "rule-scope-example-service"
 		ruleServiceNamespace := "rule-service-namespace"
 
-		ruleJwt := newJwtRuleBuilderWithDummyData().
+		ruleJwt := newNoAuthRuleBuilderWithDummyData().
 			withPath("/headers").
 			withServiceName(ruleServiceName).
 			withServiceNamespace(ruleServiceNamespace).
@@ -150,72 +147,9 @@ var _ = Describe("Processor", func() {
 		Expect(ap.Labels[processing.OwnerLabel]).To(Equal(fmt.Sprintf("%s.%s", apiRule.Name, apiRule.Namespace)))
 	})
 
-	When("additional handler to JWT", func() {
-
-		It("should create AP for noAuth with From spec having Source.Principals == cluster.local/ns/istio-system/sa/istio-ingressgateway-service-account", func() {
-			// given
-			ruleJwt := newJwtRuleBuilderWithDummyData().
-				withPath("/img").
-				addJwtAuthorizationRequiredScopes(requiredScopeA, requiredScopeB).
-				build()
-
-			ruleNoAuth := newNoAuthRuleBuilderWithDummyData().
-				withPath("/headers").
-				build()
-
-			apiRule := newAPIRuleBuilderWithDummyData().
-				withRules(ruleNoAuth, ruleJwt).
-				build()
-			svc := newServiceBuilderWithDummyData().build()
-			client := getFakeClient(svc)
-			processor := authorizationpolicy.NewProcessor(&testLogger, apiRule)
-
-			// when
-			results, err := processor.EvaluateReconciliation(context.Background(), client)
-
-			// then
-			Expect(err).To(BeNil())
-			Expect(results).To(HaveLen(2))
-
-			for _, result := range results {
-				ap := result.Obj.(*securityv1beta1.AuthorizationPolicy)
-
-				Expect(ap).NotTo(BeNil())
-				Expect(len(ap.Spec.Rules[0].To)).To(Equal(1))
-				Expect(len(ap.Spec.Rules[0].To[0].Operation.Paths)).To(Equal(1))
-
-				expectedHandlers := []string{"/headers", "/img"}
-				Expect(slices.Contains(expectedHandlers, ap.Spec.Rules[0].To[0].Operation.Paths[0])).To(BeTrue())
-
-				switch ap.Spec.Rules[0].To[0].Operation.Paths[0] {
-				case "/headers":
-					Expect(len(ap.Spec.Rules[0].From)).To(Equal(1))
-					Expect(ap.Spec.Rules[0].From[0].Source.Principals[0]).To(Equal("cluster.local/ns/istio-system/sa/istio-ingressgateway-service-account"))
-				case "/img":
-					Expect(len(ap.Spec.Rules[0].From)).To(Equal(1))
-				}
-
-				Expect(len(ap.Spec.Rules)).To(BeElementOf([]int{1, 3}))
-				if len(ap.Spec.Rules) == 3 {
-					for i := 0; i < 3; i++ {
-						Expect(ap.Spec.Rules[i].When[0].Key).To(BeElementOf(testExpectedScopeKeys))
-						Expect(ap.Spec.Rules[i].When).To(HaveLen(2))
-						Expect(ap.Spec.Rules[i].When[0].Key).To(BeElementOf(testExpectedScopeKeys))
-						Expect(ap.Spec.Rules[i].When[0].Values[0]).To(BeElementOf(requiredScopeA, requiredScopeB))
-						Expect(ap.Spec.Rules[i].When[1].Key).To(BeElementOf(testExpectedScopeKeys))
-						Expect(ap.Spec.Rules[i].When[1].Values[0]).To(BeElementOf(requiredScopeA, requiredScopeB))
-					}
-				} else {
-					Expect(len(ap.Spec.Rules)).To(Equal(1))
-				}
-			}
-		})
-	})
-
 	It("should create AP when no exists", func() {
 		// given: New resources
-
-		rule := newJwtRuleBuilderWithDummyData().build()
+		rule := newNoAuthRuleBuilderWithDummyData().build()
 		apiRule := newAPIRuleBuilderWithDummyData().
 			withRules(rule).
 			build()
