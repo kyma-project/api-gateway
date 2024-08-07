9d132289bd321650910b6cf9685b66fc021b2a77
Tim Riffer <tim.riffer@sap.com>
2024-08-07 15:16:04 +0200

    V2alpha1 RequestAuthentication processor (#1228)
    * Add Authorization Policy processing
    
    * Add JWT tests for AuthorizationPolicy
    
    * Update remaining Authorization Policy tests
    
    * Add v2alpha1 processor for RequestAuthentication
    
    * Use new processor in v2alpha1 and migration reconciliation
    
    * Restructure test files
    
    * Remove unused builder methods

--- internal/processing/processors/migration/processors.go
+++ internal/processing/processors/migration/processors.go
@@ -5,8 +5,8 @@ import (
 	gatewayv1beta1 "github.com/kyma-project/api-gateway/apis/gateway/v1beta1"
 	gatewayv2alpha1 "github.com/kyma-project/api-gateway/apis/gateway/v2alpha1"
 	"github.com/kyma-project/api-gateway/internal/processing"
-	"github.com/kyma-project/api-gateway/internal/processing/processors/istio"
 	"github.com/kyma-project/api-gateway/internal/processing/processors/v2alpha1/authorizationpolicy"
+	"github.com/kyma-project/api-gateway/internal/processing/processors/v2alpha1/requestauthentication"
 	"github.com/kyma-project/api-gateway/internal/processing/processors/v2alpha1/virtualservice"
 )
 
@@ -25,7 +25,7 @@ func NewMigrationProcessors(apiRuleV2alpha1 *gatewayv2alpha1.APIRule, apiRuleV1b
 		fallthrough // We want to also use the processors from the previous steps
 	case applyIstioAuthorizationMigrationStep: // Step 1
 		processors = append(processors, authorizationpolicy.NewProcessor(log, apiRuleV2alpha1))
-		processors = append(processors, istio.Newv1beta1RequestAuthenticationProcessor(config, apiRuleV1beta1))
+		processors = append(processors, requestauthentication.NewProcessor(apiRuleV2alpha1))
 	}
 	return processors
 }
