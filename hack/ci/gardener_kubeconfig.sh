e114768debee3ca4ace6f7a46bd886c4b7c228b1
Vladimir Videlov <vladimir.videlov@sap.com>
2024-08-20 17:42:19 +0300

    Fix gardener provision bash scripts

--- hack/ci/gardener_kubeconfig.sh
+++ hack/ci/gardener_kubeconfig.sh
@@ -1,3 +1,26 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+function check_required_vars() {
+  local requiredVarMissing=false
+  for var in "$@"; do
+    if [ -z "${!var}" ]; then
+      >&2 echo "Environment variable ${var} is required but not set"
+      requiredVarMissing=true
+    fi
+  done
+  if [ "${requiredVarMissing}" = true ] ; then
+    exit 2
+  fi
+}
+
+requiredVars=(
+    GARDENER_TOKEN
+)
+
+check_required_vars "${requiredVars[@]}"
+
 cat <<EOF > gardener_kubeconfig.yaml
 apiVersion: v1
 kind: Config
