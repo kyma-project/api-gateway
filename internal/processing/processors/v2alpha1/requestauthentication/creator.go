bb00e5bde81aaeace1d4402b70b104e98aebb4c2
Chwila <bartosz.chwila@sap.com>
2024-09-09 16:06:22 +0200

    Remove parentheses

--- internal/processing/processors/v2alpha1/requestauthentication/creator.go
+++ internal/processing/processors/v2alpha1/requestauthentication/creator.go
@@ -19,7 +19,7 @@ type requestAuthenticationCreator struct{}
 func (r requestAuthenticationCreator) Create(ctx context.Context, client client.Client, api *gatewayv2alpha1.APIRule) (map[string]*securityv1beta1.RequestAuthentication, error) {
 	requestAuthentications := make(map[string]*securityv1beta1.RequestAuthentication)
 	for _, rule := range api.Spec.Rules {
-		if rule.Jwt != nil || (rule.ExtAuth != nil && rule.ExtAuth.Restrictions != nil) {
+		if rule.Jwt != nil || rule.ExtAuth != nil && rule.ExtAuth.Restrictions != nil {
 			ra, err := generateRequestAuthentication(ctx, client, api, rule)
 			if err != nil {
 				return requestAuthentications, err
