733d056d48353fdea0076329f3c9ed76c8dace03
Tim Riffer <tim.riffer@sap.com>
2024-09-13 13:47:39 +0200

    Update integration test step selector

--- tests/integration/testsuites/v2alpha1/scenario_no_auth.go
+++ tests/integration/testsuites/v2alpha1/scenario_no_auth.go
@@ -9,6 +9,6 @@ func initNoAuthWildcard(ctx *godog.ScenarioContext, ts *testsuite) {
 
 	ctx.Step(`^NoAuthWildcard: There is a httpbin service$`, scenario.thereIsAHttpbinService)
 	ctx.Step(`^NoAuthWildcard: The APIRule is applied$`, scenario.theAPIRuleIsApplied)
-	ctx.Step(`^NoAuthWildcard: Calling the "([^"]*)" endpoint without token should result in status between (\d+) and (\d+)$`, scenario.callingTheEndpointWithoutTokenShouldResultInStatusBetween)
+	ctx.Step(`^NoAuthWildcard: Calling the "([^"]*)" endpoint without a token should result in status between (\d+) and (\d+)$`, scenario.callingTheEndpointWithoutTokenShouldResultInStatusBetween)
 	ctx.Step(`^NoAuthWildcard: Teardown httpbin service$`, scenario.teardownHttpbinService)
 }
