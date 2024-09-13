19699c8a04df63c383e45942ac07d7f1924819e0
Tim Riffer <tim.riffer@sap.com>
2024-09-13 12:46:30 +0200

    Add release notes

--- tests/integration/testsuites/v2alpha1/scenario_no_auth.go
+++ tests/integration/testsuites/v2alpha1/scenario_no_auth.go
@@ -7,8 +7,8 @@ import (
 func initNoAuthWildcard(ctx *godog.ScenarioContext, ts *testsuite) {
 	scenario := ts.createScenario("no-auth-wildcard.yaml", "no-auth-wildcard")
 
-	ctx.Step(`^JwtAndUnrestricted: There is a httpbin service$`, scenario.thereIsAHttpbinService)
-	ctx.Step(`^JwtAndUnrestricted: The APIRule is applied$`, scenario.theAPIRuleIsApplied)
-	ctx.Step(`^JwtAndUnrestricted: Calling the "([^"]*)" endpoint without token should result in status between (\d+) and (\d+)$`, scenario.callingTheEndpointWithoutTokenShouldResultInStatusBetween)
-	ctx.Step(`^JwtAndUnrestricted: Teardown httpbin service$`, scenario.teardownHttpbinService)
+	ctx.Step(`^Wildcard: There is a httpbin service$`, scenario.thereIsAHttpbinService)
+	ctx.Step(`^Wildcard: The APIRule is applied$`, scenario.theAPIRuleIsApplied)
+	ctx.Step(`^Wildcard: Calling the "([^"]*)" endpoint without token should result in status between (\d+) and (\d+)$`, scenario.callingTheEndpointWithoutTokenShouldResultInStatusBetween)
+	ctx.Step(`^Wildcard: Teardown httpbin service$`, scenario.teardownHttpbinService)
 }
