7747e3cc12baa1c0d34d9bc611ff1e20fc47c7a7
Luk, Marek <marek.luk@sap.com>
2024-10-09 12:45:35 +0200

    Revert "Better hooks - patch Istio programmatically to be able to clean it up after the test"
    This reverts commit 322aaeadcaf4779804ba019b4756bdd704cd42ed.

--- tests/integration/pkg/hooks/istio.go
+++ tests/integration/pkg/hooks/istio.go
@@ -1,27 +1,23 @@
 package hooks
 
 import (
+	"context"
 	_ "embed"
-	"fmt"
 	"github.com/avast/retry-go/v4"
+	k8sclient "github.com/kyma-project/api-gateway/tests/integration/pkg/client"
 	"github.com/kyma-project/api-gateway/tests/integration/pkg/istio"
-	"github.com/kyma-project/api-gateway/tests/integration/pkg/manifestprocessor"
-	"github.com/kyma-project/api-gateway/tests/integration/pkg/resource"
 	"github.com/kyma-project/api-gateway/tests/integration/pkg/testcontext"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
 	"log"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/yaml"
+	"time"
 )
 
-//go:embed manifests/ext-auth.yaml
-var extAuthYaml []byte
+//go:embed manifests/istio-cr.yaml
+var istioCrManifest []byte
 
 const skipVerifyJwksResolverEnvVar = "JWKS_RESOLVER_INSECURE_SKIP_VERIFY"
-const extAuthorizerName = "sample-ext-authz-http"
-const extAuthorizerPort = 8000
-const extAuthorizerService = "ext-authz.ext-auth.svc.cluster.local"
-const extAuthorizerHeader = "x-ext-authz"
 
 func IstioSkipVerifyJwksResolverSuiteHook(t testcontext.Testsuite) func() error {
 	return func() error {
@@ -36,70 +32,32 @@ func IstioSkipVerifyJwksResolverSuiteHookTeardown(t testcontext.Testsuite) func(
 	}
 }
 
-func installExternalAuthorizer(resourceMgr *resource.Manager, k8sClient dynamic.Interface) error {
-	log.Printf("Patching Istio CR with external authorizer")
-	res, err := resourceMgr.GetResource(k8sClient, schema.GroupVersionResource{Group: "operator.kyma-project.io", Version: "v1alpha2", Resource: "istios"}, "kyma-system", "default", []retry.Option{retry.Attempts(1)}...)
-	if err != nil {
-		return fmt.Errorf("Could not get Istio CR: %s", err.Error())
-	}
-
-	authorizers, _, err := unstructured.NestedSlice(res.Object, "spec", "config", "authorizers")
-	if err != nil {
-		return fmt.Errorf("Could not get authorizers: %s", err.Error())
-	}
-
-	var newAuthorizer = map[string]interface{}{
-		"name":    extAuthorizerName,
-		"port":    int64(extAuthorizerPort),
-		"service": extAuthorizerService,
-		"headers": map[string]interface{}{
-			"inCheck": map[string]interface{}{
-				"include": append(make([]interface{}, 0), extAuthorizerHeader),
-			},
-		},
-	}
-
-	authorizerFound := false
-	for i, authorizer := range authorizers {
-		authorizerName, _, err := unstructured.NestedString(authorizer.(map[string]interface{}), "name")
-		if err != nil {
-			return fmt.Errorf("Could get authorizer name: %s", err.Error())
-		}
-		if authorizerName == extAuthorizerName {
-			log.Printf("Authorizer already exists, updating it to an expected state")
-			authorizers[i] = newAuthorizer
-			authorizerFound = true
-		}
-	}
-	if !authorizerFound {
-		authorizers = append(authorizers, newAuthorizer)
-	}
-
-	err = unstructured.SetNestedSlice(res.Object, authorizers, "spec", "config", "authorizers")
-	if err != nil {
-		return fmt.Errorf("could not set authorizers: %s", err.Error())
-	}
-
-	err = resourceMgr.UpdateResource(k8sClient, schema.GroupVersionResource{Group: "operator.kyma-project.io", Version: "v1alpha2", Resource: "istios"}, "kyma-system", "default", *res)
-	if err != nil {
-		return fmt.Errorf("could not update istio CR: %s", err.Error())
-	}
-
-	log.Printf("Deploying External Authorizer")
-	resources, err := manifestprocessor.ParseYaml(extAuthYaml)
+var ApplyExtAuthorizerIstioCR = func() error {
+	log.Printf("Apply Istio CR with External Authorizer config")
+	istioCr, err := getIstioCr()
 	if err != nil {
 		return err
 	}
-
-	_, err = resourceMgr.CreateOrUpdateResources(k8sClient, resources...)
-	if err != nil {
+	client := k8sclient.GetK8sClient()
+	spec := istioCr.Object["spec"]
+	return retry.Do(func() error {
+		_, err := controllerutil.CreateOrPatch(context.Background(), client, &istioCr, func() error {
+			istioCr.Object["spec"] = spec
+			return nil
+		})
 		return err
-	}
-	return nil
+	}, []retry.Option{
+		retry.Delay(time.Duration(2) * time.Second),
+		retry.Attempts(5),
+		retry.DelayType(retry.FixedDelay),
+	}...)
 }
 
-func ExternalAuthorizerInstallHook(t testcontext.Testsuite) func() error {
-	return func() error {
-		return installExternalAuthorizer(t.ResourceManager(), t.K8sClient())
+func getIstioCr() (unstructured.Unstructured, error) {
+	var istioCr unstructured.Unstructured
+	err := yaml.Unmarshal(istioCrManifest, &istioCr)
+	if err != nil {
+		return unstructured.Unstructured{}, err
 	}
+	return istioCr, nil
 }
