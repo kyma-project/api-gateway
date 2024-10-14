cf431a587a8e93bd0d273bd32f7434e3ed14d449
Kamil Kasperski <kamil.kasperski@sap.com>
2024-10-14 09:45:42 +0200

    feat: add envoyfilter and ratelimit builders

---
+++ internal/builders/envoyfilter/envoyfilter_test.go
@@ -0,0 +1,51 @@
+package envoyfilter
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	networkingv1alpha3 "istio.io/api/networking/v1alpha3"
+	"testing"
+)
+
+var _ = Describe("EnvoyFilterBuilder", func() {
+	Context("Build", func() {
+		patch := networkingv1alpha3.EnvoyFilter_EnvoyConfigObjectPatch{
+			ApplyTo: networkingv1alpha3.EnvoyFilter_HTTP_ROUTE,
+			Patch: &networkingv1alpha3.EnvoyFilter_Patch{
+				Operation: networkingv1alpha3.EnvoyFilter_Patch_INSERT_BEFORE,
+			},
+		}
+		f := NewEnvoyFilterBuilder().
+			WithName("test").
+			WithNamespace("test").
+			WithAnnotation("test", "test").
+			WithAnnotation("foo", "bar").
+			WithWorkloadSelector("app", "test").
+			WithWorkloadSelector("foo", "bar").
+			WithConfigPatch(&patch).
+			Build()
+		It("has added name and namespace", func() {
+			Expect(f.Name).To(Equal("test"))
+			Expect(f.Namespace).To(Equal("test"))
+		})
+		It("has annotations added", func() {
+			Expect(f.Annotations).To(HaveLen(2))
+			Expect(f.Annotations).To(HaveKeyWithValue("test", "test"))
+			Expect(f.Annotations).To(HaveKeyWithValue("foo", "bar"))
+		})
+		It("has workloadSelector added", func() {
+			Expect(f.Spec.WorkloadSelector).ToNot(BeNil())
+			Expect(f.Spec.WorkloadSelector.GetLabels()).To(HaveLen(2))
+			Expect(f.Spec.WorkloadSelector.GetLabels()).To(HaveKeyWithValue("app", "test"))
+			Expect(f.Spec.WorkloadSelector.GetLabels()).To(HaveKeyWithValue("foo", "bar"))
+		})
+		It("has ConfigPatch added", func() {
+			Expect(f.Spec.ConfigPatches).To(HaveLen(1))
+		})
+	})
+})
+
+func TestEnvoyFilter(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "EnvoyFilter Package")
+}
---
+++ internal/ratelimit/ratelimit_test.go
@@ -0,0 +1,235 @@
+package ratelimit
+
+import (
+	"github.com/kyma-project/api-gateway/internal/builders/envoyfilter"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/util/json"
+	"os"
+	"sigs.k8s.io/yaml"
+	"testing"
+	"time"
+)
+
+var _ = Describe("RateLimit", func() {
+	Context("NewLocalRateLimit", func() {
+		It("should return a new RateLimit For descriptors d0, d1", func() {
+			d0 := Descriptor{
+				Entries: DescriptorEntries{
+					{Key: "x-api-version", Val: "v1"},
+					{Key: "path", Val: "/headers"},
+				},
+				Bucket: Bucket{
+					MaxTokens:     2,
+					TokensPerFill: 2,
+					FillInterval:  30 * time.Second,
+				},
+			}
+			d1 := Descriptor{
+				Entries: DescriptorEntries{
+					{Key: "path", Val: "/ip"},
+				},
+				Bucket: Bucket{
+					MaxTokens:     20,
+					TokensPerFill: 10,
+					FillInterval:  30 * time.Second,
+				},
+			}
+
+			rl := NewLocalRateLimit().
+				For(d0).
+				For(d1)
+			Expect(rl.descriptors).Should(HaveLen(2))
+			Expect(rl.actions).Should(HaveLen(2))
+		})
+		It("should have :path action when descriptor contains pseudo header 'path'", func() {
+			d := Descriptor{
+				Entries: DescriptorEntries{
+					{Key: "path", Val: "/headers"},
+				},
+				Bucket: Bucket{
+					MaxTokens:     2,
+					TokensPerFill: 2,
+					FillInterval:  30 * time.Second,
+				},
+			}
+			rl := NewLocalRateLimit().For(d)
+			Expect(rl.descriptors).Should(HaveLen(1))
+			Expect(rl.actions).Should(HaveLen(1))
+			Expect(rl.actions[0].RequestHeaders.Name).Should(Equal(":path"))
+			Expect(rl.actions[0].RequestHeaders.DescriptorKey).Should(Equal("path"))
+		})
+		It("should return a new RateLimit For defaultBucket b", func() {
+			b := Bucket{
+				MaxTokens:     10,
+				TokensPerFill: 5,
+				FillInterval:  30 * time.Second,
+			}
+			rl := NewLocalRateLimit().WithDefaultBucket(b)
+			Expect(rl.defaultBucket).Should(Equal(b))
+		})
+	})
+	Context("AddToEnvoyFilter", func() {
+		b := Bucket{
+			MaxTokens:     10,
+			TokensPerFill: 5,
+			FillInterval:  30 * time.Second,
+		}
+		d0 := Descriptor{
+			Entries: DescriptorEntries{
+				{Key: "x-api-version", Val: "v1"},
+				{Key: "path", Val: "/headers"},
+			},
+			Bucket: Bucket{
+				MaxTokens:     2,
+				TokensPerFill: 2,
+				FillInterval:  30 * time.Second,
+			},
+		}
+		d1 := Descriptor{
+			Entries: DescriptorEntries{
+				{Key: "path", Val: "/ip"},
+			},
+			Bucket: Bucket{
+				MaxTokens:     20,
+				TokensPerFill: 10,
+				FillInterval:  30 * time.Second,
+			},
+		}
+		It("adds 2 ConfigPatches to the EnvoyFilterBuilder", func() {
+			builder := envoyfilter.NewEnvoyFilterBuilder().
+				WithName("httpbin-local-rate-limit").
+				WithNamespace("default").
+				WithWorkloadSelector("app", "httpbin")
+			rl := NewLocalRateLimit().
+				For(d1).
+				For(d0).
+				WithDefaultBucket(b)
+			rl.AddToEnvoyFilter(builder)
+			Expect(builder.ConfigPatches).To(HaveLen(2))
+		})
+	})
+	Context("RateLimitConfigPatch", func() {
+		d0 := Descriptor{
+			Entries: DescriptorEntries{
+				{Key: "x-api-version", Val: "v1"},
+				{Key: "path", Val: "/headers"},
+			},
+			Bucket: Bucket{
+				MaxTokens:     2,
+				TokensPerFill: 2,
+				FillInterval:  30 * time.Second,
+			},
+		}
+		defaultBucket := Bucket{
+			MaxTokens:     20,
+			TokensPerFill: 10,
+			FillInterval:  30 * time.Second,
+		}
+		rl := NewLocalRateLimit().For(d0).WithDefaultBucket(defaultBucket)
+		config := rl.RateLimitConfigPatch()
+		It("returns ConfigPatch with 2 route actions", func() {
+			vals := config.Patch.Value.GetFields()
+			rateLimits := vals["route"].GetStructValue().GetFields()["rate_limits"].GetListValue().GetValues()
+			Expect(rateLimits).Should(HaveLen(1))
+			actions := rateLimits[0].GetStructValue().GetFields()["actions"].GetListValue().GetValues()
+			Expect(actions).Should(HaveLen(2))
+
+			exp1 := Action{RequestHeaders: RequestHeader{Name: "x-api-version", DescriptorKey: "x-api-version"}}
+			Expect(actions).Should(ContainElement(exp1.Value()))
+			exp2 := Action{RequestHeaders: RequestHeader{Name: ":path", DescriptorKey: "path"}}
+			Expect(actions).Should(ContainElement(exp2.Value()))
+		})
+		It("returns ConfigPatch With Default bucket", func() {
+			vals := config.Patch.Value.GetFields()
+			config := vals["typed_per_filter_config"].GetStructValue().GetFields()[LocalRateLimitFilterName]
+			Expect(config).ShouldNot(BeNil())
+			gotBucket := config.GetStructValue().GetFields()["value"].GetStructValue().GetFields()["token_bucket"]
+			expBucket := Bucket{
+				MaxTokens:     20,
+				TokensPerFill: 10,
+				FillInterval:  30 * time.Second,
+			}
+			Expect(gotBucket).Should(Equal(expBucket.Value()))
+		})
+		It("returns ConfigPatch with 1 descriptor", func() {
+			vals := config.Patch.Value.GetFields()
+			config := vals["typed_per_filter_config"].GetStructValue().GetFields()[LocalRateLimitFilterName]
+			Expect(config).ShouldNot(BeNil())
+			gotDesc := config.GetStructValue().GetFields()["value"].GetStructValue().GetFields()["descriptors"].GetListValue().GetValues()
+			Expect(gotDesc).Should(HaveLen(1))
+
+			expDesc := Descriptor{
+				Entries: DescriptorEntries{
+					{Key: "x-api-version", Val: "v1"},
+					{Key: "path", Val: "/headers"},
+				},
+				Bucket: Bucket{
+					MaxTokens:     2,
+					TokensPerFill: 2,
+					FillInterval:  30 * time.Second,
+				},
+			}
+
+			Expect(gotDesc).Should(ContainElement(expDesc.Value()))
+		})
+	})
+	Context("RateLimit to EnvoyFilter conversion", func() {
+		d0 := Descriptor{
+			Entries: DescriptorEntries{
+				{Key: "x-api-version", Val: "v1"},
+				{Key: "path", Val: "/headers"},
+			},
+			Bucket: Bucket{
+				MaxTokens:     2,
+				TokensPerFill: 2,
+				FillInterval:  30 * time.Second,
+			},
+		}
+		d1 := Descriptor{
+			Entries: DescriptorEntries{
+				{Key: "path", Val: "/ip"},
+			},
+			Bucket: Bucket{
+				MaxTokens:     20,
+				TokensPerFill: 10,
+				FillInterval:  30 * time.Second,
+			},
+		}
+		bucket := Bucket{
+			MaxTokens:     10,
+			TokensPerFill: 5,
+			FillInterval:  30 * time.Second,
+		}
+		rl := NewLocalRateLimit().
+			For(d1).
+			For(d0).
+			WithDefaultBucket(bucket)
+		builder := envoyfilter.NewEnvoyFilterBuilder().
+			WithName("httpbin-local-rate-limit").
+			WithNamespace("default").
+			WithWorkloadSelector("app", "httpbin")
+		rl.AddToEnvoyFilter(builder)
+		It("builds EnvoyFilter with expected configuration", func() {
+			f, err := os.ReadFile("testdata/envoy_patches.yaml")
+			Expect(err).Should(Succeed())
+			var fi []envoyfilter.ConfigPatch
+			exp, err := yaml.YAMLToJSON(f)
+			Expect(err).To(Succeed())
+			Expect(json.Unmarshal(exp, &fi)).Should(Succeed())
+			result := builder.Build()
+			got, err := json.Marshal(result.Spec.ConfigPatches)
+			Expect(err).Should(Succeed())
+			// This may fail if struct gets marshalled in not expected order.
+			// However, I don't have much idea how to cover if marshaled structure is compatible with Envoy's
+			// So just make sure the testdata is ordered.
+			// Probably EnvTest would be better.
+			Expect(got).Should(Equal(exp))
+		})
+	})
+})
+
+func TestRateLimitSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "RateLimit Suite")
+}
