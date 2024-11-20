7ad7ec0db313ec9bc88b55f04a850e208f9ef8f2
mluk-sap <40407813+mluk-sap@users.noreply.github.com>
2024-11-20 09:56:14 +0100

    Fixed an option to use an oauth2 mock in custom domain tests (#1464)

--- tests/integration/env_vars_custom_domain.sh
+++ tests/integration/env_vars_custom_domain.sh
@@ -17,19 +17,16 @@ if [[ -z ${TEST_SA_ACCESS_KEY_PATH} ]]; then
   exit 2
 fi
 
-if [[ -z ${OIDC_CONFIG_URL} ]]; then
-  >&2 echo "Environment variable OIDC_CONFIG_URL is required but not set"
-  exit 2
-fi
+if [[ -n ${OIDC_CONFIG_URL} ]]; then
+  if [[ -z ${CLIENT_ID} ]]; then
+    >&2 echo "Environment variable CLIENT_ID is required when OIDC_CONFIG_URL is set"
+    exit 2
+  fi
 
-if [[ -z ${CLIENT_ID} ]]; then
-  >&2 echo "Environment variable CLIENT_ID is required but not set"
-  exit 2
-fi
-
-if [[ -z ${CLIENT_SECRET} ]]; then
-  >&2 echo "Environment variable CLIENT_SECRET is required but not set"
-  exit 2
+  if [[ -z ${CLIENT_SECRET} ]]; then
+    >&2 echo "Environment variable CLIENT_SECRET is required when OIDC_CONFIG_URL is set"
+    exit 2
+  fi
 fi
 
 export TEST_OIDC_CONFIG_URL="${OIDC_CONFIG_URL}"
