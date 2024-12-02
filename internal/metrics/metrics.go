b1bee9b9580ac5910b6b9e248e87aaa207078b57
Patryk Strugacz <patryk.strugacz@sap.com>
2024-12-02 14:24:23 +0100

    Register metrics

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -1,19 +1,25 @@
 package metrics
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"github.com/prometheus/client_golang/prometheus"
+
+	ctrlmetrics "sigs.k8s.io/controller-runtime/pkg/metrics"
+)
 
 type ApiGatewayMetrics struct {
 	apiRuleObjectModifiedErrorsCounter prometheus.Counter
 }
 
 func NewApiGatewayMetrics() *ApiGatewayMetrics {
-	return &ApiGatewayMetrics{
+	apiGatewayMetrics := &ApiGatewayMetrics{
 		apiRuleObjectModifiedErrorsCounter: prometheus.NewCounter(prometheus.CounterOpts{
 			Name:      "api_rule_object_modified_errors_total",
 			Namespace: "api_gateway",
 			Help:      "The total number of errors that occurred while modifying the APIRule object",
 		}),
 	}
+	ctrlmetrics.Registry.MustRegister(apiGatewayMetrics.apiRuleObjectModifiedErrorsCounter)
+	return apiGatewayMetrics
 }
 
 func (m *ApiGatewayMetrics) IncreaseApiRuleObjectModifiedErrorsCounter() {
