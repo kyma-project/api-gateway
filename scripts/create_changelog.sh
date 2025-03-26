e1cdfffcce5fda53a8830f2bd0dec7ef69e6ffc7
Kepka <kasia.kepka23@gmail.com>
2025-03-26 11:06:49 +0100

    Release process part2

--- scripts/create_changelog.sh
+++ scripts/create_changelog.sh
@@ -1,23 +1,38 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-LATEST_RELEASE=$2 # for testability
+set -e
 
-# standard bash error handling
-set -o nounset  # treat unset variables as an error and exit immediately.
-set -o errexit  # exit immediately when a command fails.
-set -E          # needs to be set if we want the ERR trap
-set -o pipefail # prevents errors in a pipeline from being masked
-
+# Ensure RELEASE_TAG is provided
 RELEASE_TAG=$1
+if [ -z "$RELEASE_TAG" ]; then
+  echo "Error: RELEASE_TAG is not set."
+  exit 1
+fi
 
+# Set default repository if not provided
 REPOSITORY=${REPOSITORY:-kyma-project/api-gateway}
-GITHUB_URL=https://api.github.com/repos/${REPOSITORY}
+GITHUB_URL="https://api.github.com/repos/${REPOSITORY}"
 GITHUB_AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 CHANGELOG_FILE="CHANGELOG.md"
 
-if [ "${LATEST_RELEASE}"  == "" ]
-then
-  LATEST_RELEASE=$(curl -H "${GITHUB_AUTH_HEADER}" -sS "${GITHUB_URL}/releases/latest" | jq -r '.tag_name')
+# Extract major, minor, and patch version numbers
+set -- ${RELEASE_TAG//./ }
+MAJOR=$1
+MINOR=$2
+PATCH=$3
+
+# Fetch all release tags
+TAGS=$(curl -s -H "$GITHUB_AUTH_HEADER" "$GITHUB_URL/releases" | grep -o '"tag_name": "[^"]*"' | sed -E 's/"tag_name": "([^"]*)"/\1/' | sort -V)
+
+# Determine the previous release based on versioning rules
+LATEST_TAG=""
+if [ $PATCH -ne 0 ]; then
+  LATEST_TAG=$(echo "$TAGS" | grep -E "${MAJOR}\.${MINOR}\." | tail -1 )
+elif [ "$MINOR" -ne 0 ]; then
+  LATEST_TAG=$(echo "$TAGS" | grep -E "${MAJOR}\." | tail -1 )
+else
+  LATEST_TAG=$(echo "$TAGS" | grep -E "[0-9]+\.[0-9]+\.[0-9]+$" | tail -1 )
 fi
 
-echo -e "\n**Full changelog:** https://github.com/$REPOSITORY/compare/${LATEST_RELEASE}...${RELEASE_TAG}" >> ${CHANGELOG_FILE}
+# Fetch commit history between the previous and current release
+echo -e "\n**Full changelog:** https://github.com/$REPOSITORY/compare/${LATEST_TAG}...${RELEASE_TAG}" >> ${CHANGELOG_FILE}
