197d2b7b9998ec17975f27ae3d7c1f2c5fa656e1
Robert Kubica <168091212+kubicar@users.noreply.github.com>
2025-04-17 14:37:24 +0200

    feat(apirule): add default annotation handling and rename admission webhook file

--- apis/gateway/v2alpha1/apirule_mutation.go
+++ apis/gateway/v2alpha1/apirule_mutation.go
@@ -10,6 +10,11 @@ type MutatingWebhook struct {
 
 func (in *MutatingWebhook) Default(_ context.Context, obj runtime.Object) error {
 	apiRule := obj.(*APIRule)
+	// TODO add logger
+
+	if apiRule.Annotations == nil {
+		apiRule.Annotations = map[string]string{}
+	}
 
 	if _, ok := apiRule.Annotations["gateway.kyma-project.io/original-version"]; !ok {
 		// need to set original-version to v2alpha1	because we need to know version specifier for Busola list view three tabs
