09d8d86f93b277e4e9c30761c27d72c0c12f9e04
Bartosz Chwila <103247439+barchw@users.noreply.github.com>
2025-04-30 08:46:45 +0200

    Remove unused variables

--- performance_tests/scripts/oauth-post-test.js
+++ performance_tests/scripts/oauth-post-test.js
@@ -1,10 +1,10 @@
 import {post, getToken, postFlow} from './common.js';
 
-const CLIENT_ID = '5ffee54b-f82f-4328-a055-ff75fcc1fe85';
-const CLIENT_SECRET = 'su_D~HHsCAE8-UUb_pOE6XpYRQ';
+const CLIENT_ID = `${ CLIENT_ID }`;
+const CLIENT_SECRET = `${ CLIENT_SECRET }`;
 const SCOPES = 'read';
-const AUTH_URL = 'https://oauth2.ks-kyma-806s.goatz.shoot.canary.k8s-hana.ondemand.com/oauth2/token';
-const API_URL = 'https://hello-world-oauth2.ks-kyma-806s.goatz.shoot.canary.k8s-hana.ondemand.com/dispatch';
+const AUTH_URL = `${ AUTH_URL }`;
+const API_URL = `${ API_URL }`;
 
 const REQUEST_BODY = {
   scope: SCOPES,
