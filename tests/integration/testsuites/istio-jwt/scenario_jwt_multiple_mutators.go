4c36f3bac4b5e3eb1a60262b3628142da85b1b9c
Kamil Kasperski <kamil.kasperski@sap.com>
2025-06-06 11:28:47 +0200

    istio-jwt manifests

--- tests/integration/testsuites/istio-jwt/scenario_jwt_multiple_mutators.go
+++ tests/integration/testsuites/istio-jwt/scenario_jwt_multiple_mutators.go
@@ -2,9 +2,10 @@ package istiojwt
 
 import (
 	"fmt"
-	"github.com/kyma-project/api-gateway/tests/integration/pkg/testcontext"
 	"net/http"
 	"strings"
+
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/testcontext"
 
 	"github.com/cucumber/godog"
 	"github.com/kyma-project/api-gateway/tests/integration/pkg/helpers"
@@ -14,28 +15,10 @@ func initMultipleMutators(ctx *godog.ScenarioContext, ts *testsuite) {
 	scenario := ts.createScenario("istio-jwt-multiple-mutators.yaml", "istio-jwt-multiple-mutators")
 
 	ctx.Step(`^JwtMultipleMutators: There is a httpbin service$`, scenario.thereIsAHttpbinService)
-	ctx.Step(`^JwtMultipleMutators: There is an endpoint on path "([^"]*)" with a header mutator setting "([^"]*)" header to "([^"]*)" and "([^"]*)" header to "([^"]*)"$`, scenario.thereIsAnEndpointWithHeaderMutatorWithTwoHeaders)
-	ctx.Step(`^JwtMultipleMutators: There is an endpoint on path "([^"]*)" with a cookie mutator setting "([^"]*)" cookie to "([^"]*)" and "([^"]*)" cookie to "([^"]*)"$`, scenario.thereIsAnEndpointWithCookieMutatorWithTwoCookies)
 	ctx.Step(`^JwtMultipleMutators: The APIRule is applied$`, scenario.theAPIRuleIsApplied)
 	ctx.Step(`^JwtMultipleMutators: Calling the "([^"]*)" endpoint should return response with cookies "([^"]*)" with value "([^"]*)" and "([^"]*)" with value "([^"]*)"$`, scenario.shouldReturnResponseWithKeyValuePairs)
 	ctx.Step(`^JwtMultipleMutators: Calling the "([^"]*)" endpoint should return response with headers "([^"]*)" with value "([^"]*)" and "([^"]*)" with value "([^"]*)"$`, scenario.shouldReturnResponseWithKeyValuePairs)
 	ctx.Step(`^JwtMultipleMutators: Teardown httpbin service$`, scenario.teardownHttpbinService)
-}
-
-func (s *scenario) thereIsAnEndpointWithHeaderMutatorWithTwoHeaders(_, header1, header1Value, header2, header2Value string) error {
-	s.ManifestTemplate["header1"] = header1
-	s.ManifestTemplate["header1Value"] = header1Value
-	s.ManifestTemplate["header2"] = header2
-	s.ManifestTemplate["header2Value"] = header2Value
-	return nil
-}
-
-func (s *scenario) thereIsAnEndpointWithCookieMutatorWithTwoCookies(_, cookie1, cookie1Value, cookie2, cookie2Value string) error {
-	s.ManifestTemplate["cookie1"] = cookie1
-	s.ManifestTemplate["cookie1Value"] = cookie1Value
-	s.ManifestTemplate["cookie2"] = cookie2
-	s.ManifestTemplate["cookie2Value"] = cookie2Value
-	return nil
 }
 
 func (s *scenario) shouldReturnResponseWithKeyValuePairs(endpoint, k1, v1, k2, v2 string) error {
