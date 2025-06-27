8f5a48273528a97d564955a6e76787891ed975ff
Kamil Kasperski <kamil.kasperski@sap.com>
2025-06-27 10:16:47 +0200

    chore: remove UI tests
    Eventually we will write something new if we migrate to microfrontends... maybe.

--- tests/ui/tests/support/k8sclient/index.ts
+++
@@ -1,7 +0,0 @@
-import "./namespace"
-import "./service"
-import "./apiRule";
-export {ApiRuleConfig, ApiRuleAccessStrategy} from "./apiRule";
-export {Commands as K8sClientCommands} from "./commands";
-export {KubernetesConfig, getK8sCurrentContext} from "./kubeconfig";
-
--- tests/ui/tests/support/status.ts
+++
@@ -1,5 +0,0 @@
-export type Status = "OK" | "ERROR" | "WARNING";
-
-Cypress.Commands.add('hasStatusLabel', (status: Status) : void => {
-    cy.get(`[aria-label="Status"]:visible`).contains(status);
-});
