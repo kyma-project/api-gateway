83eab1308b0207c68dc36c8f4a9a575112a4bc60
Robert Kubica <168091212+kubicar@users.noreply.github.com>
2025-07-24 15:04:17 +0200

    fix: local rate limiting targeting Istio ingress gateway (#2064)
    * fix: local rate limiting targeting Istio ingress gateway
    * fix: improve RateLimit resource handling and deletion logic in api-gateway e2e
    
    * Update docs/user/custom-resources/ratelimit/04-00-ratelimit.md
    
    Co-authored-by: Natalia Sitko <80401180+nataliasitko@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Natalia Sitko <80401180+nataliasitko@users.noreply.github.com>

--- tests/integration/pkg/hooks/api-gateway.go
+++ tests/integration/pkg/hooks/api-gateway.go
@@ -13,15 +13,17 @@ import (
 
 	"github.com/avast/retry-go/v4"
 	"github.com/cucumber/godog"
-	"github.com/kyma-project/api-gateway/apis/gateway/v1beta1"
-	"github.com/kyma-project/api-gateway/apis/operator/v1alpha1"
-	k8sclient "github.com/kyma-project/api-gateway/tests/integration/pkg/client"
-	"github.com/kyma-project/api-gateway/tests/integration/pkg/testcontext"
 	oryv1alpha1 "github.com/ory/oathkeeper-maester/api/v1alpha1"
 	"github.com/pkg/errors"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/yaml"
+
+	ratelimit "github.com/kyma-project/api-gateway/apis/gateway/ratelimit/v1alpha1"
+	"github.com/kyma-project/api-gateway/apis/gateway/v1beta1"
+	"github.com/kyma-project/api-gateway/apis/operator/v1alpha1"
+	k8sclient "github.com/kyma-project/api-gateway/tests/integration/pkg/client"
+	"github.com/kyma-project/api-gateway/tests/integration/pkg/testcontext"
 
 	networkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
 )
@@ -328,6 +330,25 @@ func deleteBlockingResources(ctx context.Context) error {
 		}
 	}
 
+	rateLimitList := ratelimit.RateLimitList{}
+	err = k8sClient.List(ctx, &rateLimitList)
+	if err != nil {
+		return err
+	}
+
+	for _, rateLimit := range rateLimitList.Items {
+		err = retry.Do(func() error {
+			err := k8sClient.Delete(ctx, &rateLimit)
+			if client.IgnoreNotFound(err) != nil {
+				return fmt.Errorf("failed to delete RateLimit %s", rateLimit.GetName())
+			}
+			return nil
+		}, testcontext.GetRetryOpts()...)
+		if err != nil {
+			return err
+		}
+	}
+
 	oryRuleList := oryv1alpha1.RuleList{}
 	err = k8sClient.List(ctx, &oryRuleList)
 	if err == nil {
@@ -344,7 +365,6 @@ func deleteBlockingResources(ctx context.Context) error {
 			}
 		}
 	}
-
 	return nil
 }
 
