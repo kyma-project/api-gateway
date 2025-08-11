fcc9e8936633249093fb8afb6a83bbc4f7233098
Luk, Marek <marek.luk@sap.com>
2025-08-11 16:58:51 +0200

    Refactored ory zero downtime migration tests (cherry pick of PR 2113)

--- tests/integration/testsuites/ory/scenario_migration_allow_v1beta1.go
+++ tests/integration/testsuites/ory/scenario_migration_allow_v1beta1.go
@@ -7,10 +7,20 @@ import (
 func initMigrationAllowV1beta1(ctx *godog.ScenarioContext, ts *testsuite) {
 	scenario := ts.createScenario("migration-allow-v1beta1.yaml", "migration-allow-v1beta1")
 
+	// This structure holds all Tester instances
+	// every test scenario should have an own instance to allow parallel execution
+	zd := ZeroDowntimeTestRunner{
+		jwtConfig: scenario.jwtConfig,
+		host:      scenario.GetHostUnderTest(),
+	}
+	ctx.After(zd.CleanZeroDowntimeTests)
+
 	ctx.Step(`^migrationAllowV1beta1: There is a httpbin service with Istio injection enabled$`, scenario.thereIsAHttpbinServiceWithIstioInjection)
 	ctx.Step(`^migrationAllowV1beta1: The APIRule is applied$`, scenario.theAPIRuleIsApplied)
 	ctx.Step(`^migrationAllowV1beta1: The APIRule is updated using manifest "([^"]*)"$`, scenario.theAPIRuleIsUpdated)
 	ctx.Step(`^migrationAllowV1beta1: APIRule has status "([^"]*)"$`, scenario.theAPIRuleHasStatus)
 	ctx.Step(`^migrationAllowV1beta1: Calling the "([^"]*)" endpoint without a token should result in status between (\d+) and (\d+)$`, scenario.callingTheEndpointWithoutTokenShouldResultInStatusBetween)
 	ctx.Step(`^migrationAllowV1beta1: Resource of Kind "([^"]*)" owned by APIRule exists$`, scenario.resourceOwnedByApiRuleExists)
+	ctx.Step(`^migrationAllowV1beta1: There are continuous requests to path "([^"]*)"`, zd.StartZeroDowntimeTest)
+	ctx.Step(`^migrationAllowV1beta1: All continuous requests should succeed`, zd.FinishZeroDowntimeTests)
 }
