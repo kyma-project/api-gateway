7d1538f538c1fdc64505e05610e4f8ddddad30aa
Chwila <bartosz.chwila@sap.com>
2025-08-21 13:46:48 +0200

    - Exclude operator originated requests from validation - Add metrics for APIGateway v1beta1 access statistics

--- apis/gateway/v1beta1/apirule_webhook.go
+++ apis/gateway/v1beta1/apirule_webhook.go
@@ -19,16 +19,36 @@ package v1beta1
 import (
 	"context"
 	"errors"
+	"github.com/prometheus/client_golang/prometheus"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	ctrlmetrics "sigs.k8s.io/controller-runtime/pkg/metrics"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 // log is for logging in this package.
 var apirulelog = logf.Log.WithName("apirule-resource")
 
+var (
+	v1beta1CreationCounter prometheus.Counter
+	v1beta1UpdateCounter   prometheus.Counter
+)
+
 func (ruleV1 *APIRule) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	v1beta1CreationCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name:      "apirule_v1beta1_creation_actions_total",
+		Namespace: "api_gateway",
+		Help:      "The total number of APIRule v1beta1 CREATE actions received",
+	})
+	v1beta1UpdateCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name:      "apirule_v1beta1_update_actions_total",
+		Namespace: "api_gateway",
+		Help:      "The total number of APIRule v1beta1 UPDATE actions received",
+	})
+
+	ctrlmetrics.Registry.MustRegister(v1beta1CreationCounter)
+	ctrlmetrics.Registry.MustRegister(v1beta1UpdateCounter)
 	return ctrl.NewWebhookManagedBy(mgr).
 		For(ruleV1).
 		WithValidator(&ValidatingWebhook{}).
@@ -37,25 +57,21 @@ func (ruleV1 *APIRule) SetupWebhookWithManager(mgr ctrl.Manager) error {
 
 //+kubebuilder:webhook:path=/validate-gateway-kyma-project-io-v1beta1-apirule,mutating=false,failurePolicy=fail,sideEffects=None,groups=gateway.kyma-project.io,resources=apirules,verbs=create;update,versions=v1beta1,name=v1beta1-admission.apirule.gateway.kyma-project.io,admissionReviewVersions=v1,servicePort=9443,serviceName=api-gateway-webhook-service,serviceNamespace=kyma-system,matchPolicy=Exact
 
-type ValidatingWebhook struct {}
+type ValidatingWebhook struct{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
-func (w *ValidatingWebhook ) ValidateCreate(_ context.Context, o runtime.Object) (admission.Warnings, error) {
-	apirulelog.Info("validate create, object", "object", o)
-
+func (w *ValidatingWebhook) ValidateCreate(_ context.Context, o runtime.Object) (admission.Warnings, error) {
+	v1beta1CreationCounter.Inc()
 	return nil, errors.New("v1beta1 APIRule version is no longer supported, please use v2 instead")
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
-func (w *ValidatingWebhook)  ValidateUpdate(_ context.Context, _, _ runtime.Object) (admission.Warnings, error) {
-	apirulelog.Info("validate update")
-
+func (w *ValidatingWebhook) ValidateUpdate(_ context.Context, _, _ runtime.Object) (admission.Warnings, error) {
+	v1beta1UpdateCounter.Inc()
 	return nil, errors.New("v1beta1 APIRule version is no longer supported, please use v2 instead")
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (w *ValidatingWebhook) ValidateDelete(_ context.Context, _ runtime.Object) (admission.Warnings, error) {
-	apirulelog.Info("validate delete")
-
 	return nil, nil
 }
