48f588c78b6b259ffa1f24942188adc4cdde973c
Bartosz Chwila <103247439+barchw@users.noreply.github.com>
2025-10-14 10:13:50 +0200

    APIRule access for Gardener (#2292)
    * APIRule access for Gardener
    
    * Avoid issues with pull-request-target
    
    * Use vars
    
    * Remove usage of vars
    
    * Hardcode local kyma dev
    
    * Remove env from actions

--- internal/access/access_test.go
+++ internal/access/access_test.go
@@ -17,19 +17,58 @@ import (
 )
 
 // Shoot info with domain: test-shoot.com
+//
 //go:embed shoot_info_cm.yaml
 var shootInfoCMYAML []byte
 
 // Access config map with signature for domain: test-shoot.com
+//
 //go:embed access_cm.yaml
 var accessCMYAML []byte
 
+// Access config map with signature for wildcard domain: *.test-shoot.com
+//
+//go:embed access_cm_wildcard.yaml
+var accessCMWildcardYAML []byte
+
 // Access config map with signature for domain: test-shoot.wrong
+//
 //go:embed access_cm_wrong_domain.yaml
 var accessCMWrongDomainYAML []byte
 
 func TestShouldAllowAccessToV1Beta1(t *testing.T) {
-	t.Run("should allow access to v1beta1 when signature is valid", func(t *testing.T) {
+	t.Run("should allow access to v1beta1 for wildcard domain", func(t *testing.T) {
+		var shootInfoCM, accessCM unstructured.Unstructured
+		shootInfoCM.SetGroupVersionKind(schema.GroupVersionKind{
+			Group:   "",
+			Version: "v1",
+			Kind:    "ConfigMap",
+		})
+		accessCM.SetGroupVersionKind(schema.GroupVersionKind{
+			Group:   "",
+			Version: "v1",
+			Kind:    "ConfigMap",
+		})
+
+		err := yaml.Unmarshal(shootInfoCMYAML, &shootInfoCM.Object)
+		require.NoError(t, err)
+		err = yaml.Unmarshal(accessCMWildcardYAML, &accessCM.Object)
+		require.NoError(t, err)
+
+		k8sClient := createFakeClient(t, &shootInfoCM, &accessCM)
+		allowed, err := ShouldAllowAccessToV1Beta1(context.Background(), k8sClient)
+		assert.NoError(t, err)
+		assert.True(t, allowed)
+
+		// Update the shoot info to another domain that matches the wildcard
+		shootInfoCM.Object["data"].(map[string]interface{})["domain"] = "sub.test-shoot.com"
+		k8sClient = createFakeClient(t, &shootInfoCM, &accessCM)
+		allowed, err = ShouldAllowAccessToV1Beta1(context.Background(), k8sClient)
+		assert.NoError(t, err)
+		assert.True(t, allowed)
+	})
+
+	t.Run("should allow access to v1beta1 when signature is valid, and not to a wildcard domain", func(t *testing.T) {
 		var shootInfoCM, accessCM unstructured.Unstructured
 		shootInfoCM.SetGroupVersionKind(schema.GroupVersionKind{
 			Group:   "",
@@ -51,6 +90,13 @@ func TestShouldAllowAccessToV1Beta1(t *testing.T) {
 		allowed, err := ShouldAllowAccessToV1Beta1(context.Background(), k8sClient)
 		assert.NoError(t, err)
 		assert.True(t, allowed)
+
+		// Update the shoot info to a subdomain that does not match the exact domain
+		shootInfoCM.Object["data"].(map[string]interface{})["domain"] = "sub.test-shoot.com"
+		k8sClient = createFakeClient(t, &shootInfoCM, &accessCM)
+		allowed, err = ShouldAllowAccessToV1Beta1(context.Background(), k8sClient)
+		assert.NoError(t, err)
+		assert.False(t, allowed)
 	})
 
 	t.Run("should disallow access to v1beta1 when domain does not match", func(t *testing.T) {
