684888b98070107d5434ca1ac358458d5e21c4c4
Natalia Sitko <80401180+nataliasitko@users.noreply.github.com>
2025-11-13 14:55:16 +0100

    Update Vitepress sidebar (#2358)
    * Update Vitepress sidebar
    
    * Apply suggestions from code review
    
    Co-authored-by: Asia Bodora <72557865+joanbod@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Asia Bodora <72557865+joanbod@users.noreply.github.com>

--- docs/user/_sidebar.ts
+++ docs/user/_sidebar.ts
@@ -1,46 +1,19 @@
 export default [
   { text: 'Quick Start Guide', link: './os-quick-start.md' },
   { text: 'Tutorials', link: './tutorials/README', collapsed: true, items: [
-    { text: 'Create a Workload', link: './tutorials/01-00-create-workload.md' },
     { text: 'Set Up a Custom Domain', link: './tutorials/01-10-setup-custom-domain-for-workload.md' },
     { text: 'Set Up a TLS Gateway', link: './tutorials/01-20-set-up-tls-gateway.md' },
     { text: 'Set Up an mTLS Gateway', link: './tutorials/01-30-set-up-mtls-gateway.md' },
     { text: 'Expose a Workload', link: './tutorials/01-40-expose-workload/README.md', collapsed: true, items: [
-      { text: 'Use APIRule v2', collapsed: true, items: [
-        { text: 'Expose a Workload', link: './tutorials/01-40-expose-workload/01-40-expose-workload-apigateway.md' },
-        { text: 'Expose Multiple Workloads', link: './tutorials/01-40-expose-workload/01-41-expose-multiple-workloads.md' },
-        { text: 'Expose Workloads in Multiple Namespaces', link: './tutorials/01-40-expose-workload/01-42-expose-workloads-multiple-namespaces.md' },
-        { text: 'Use a Short Host', link: './tutorials/01-40-expose-workload/01-43-expose-workload-short-host-name.md' }
-      ]},
-      { text: 'Use APIRule v2alpha1', collapsed: true, items: [
-        { text: 'Expose a Workload', link: './tutorials/01-40-expose-workload/v2alpha1/01-40-expose-workload-apigateway.md' },
-        { text: 'Expose Multiple Workloads', link: './tutorials/01-40-expose-workload/v2alpha1/01-41-expose-multiple-workloads.md' },
-        { text: 'Expose Workloads in Multiple Namespaces', link: './tutorials/01-40-expose-workload/v2alpha1/01-42-expose-workloads-multiple-namespaces.md' },
-        { text: 'Use a Short Host', link: './tutorials/01-40-expose-workload/v2alpha1/01-43-expose-workload-short-host-name.md' }
-      ]},
-      { text: 'Use APIRule v1beta1', collapsed: true, items: [
-        { text: 'Expose a Workload', link: './tutorials/01-40-expose-workload/v1beta1-deprecated/01-40-expose-workload-apigateway.md' },
-        { text: 'Expose Multiple Workloads', link: './tutorials/01-40-expose-workload/v1beta1-deprecated/01-41-expose-multiple-workloads.md' },
-        { text: 'Expose Workloads in Multiple Namespaces', link: './tutorials/01-40-expose-workload/v1beta1-deprecated/01-42-expose-workloads-multiple-namespaces.md' }
-      ]},
+      { text: 'Expose a Workload', link: './tutorials/01-40-expose-workload/01-40-expose-workload-apigateway.md' },
+      { text: 'Expose Multiple Workloads', link: './tutorials/01-40-expose-workload/01-41-expose-multiple-workloads.md' },
+      { text: 'Expose Workloads in Multiple Namespaces', link: './tutorials/01-40-expose-workload/01-42-expose-workloads-multiple-namespaces.md' },
+      { text: 'Use a Short Host', link: './tutorials/01-40-expose-workload/01-43-expose-workload-short-host-name.md' }
     ]},
     { text: 'Expose and Secure a Workload', link: './tutorials/01-50-expose-and-secure-a-workload/README.md', collapsed: true, items: [
-      { text: 'Use APIRule v2', collapsed: true, items: [
-        { text: 'Get JSON Web Tokens (JWT)', link: './tutorials/01-50-expose-and-secure-a-workload/01-51-get-jwt.md' },
-        { text: 'Secure a Workload with JWT', link: './tutorials/01-50-expose-and-secure-a-workload/01-52-expose-and-secure-workload-jwt.md' },
-        { text: 'Secure a Workload with extAuth', link: './tutorials/01-50-expose-and-secure-a-workload/01-53-expose-and-secure-workload-ext-auth.md' },
-      ]},
-      { text: 'Use APIRule v2alpha1', collapsed: true, items: [
-        { text: 'Get JSON Web Tokens (JWT)', link: './tutorials/01-50-expose-and-secure-a-workload/v2alpha1/01-51-get-jwt.md' },
-        { text: 'Secure a Workload with JWT', link: './tutorials/01-50-expose-and-secure-a-workload/v2alpha1/01-52-expose-and-secure-workload-jwt.md' },
-        { text: 'Secure a Workload with extAuth', link: './tutorials/01-50-expose-and-secure-a-workload/v2alpha1/01-53-expose-and-secure-workload-ext-auth.md' },
-      ]},
-      { text: 'Use APIRule v1beta1', collapsed: true, items: [
-        { text: 'Secure a Workload with OAuth2', link: './tutorials/01-50-expose-and-secure-a-workload/v1beta1-deprecated/01-50-expose-and-secure-workload-oauth2.md' },
-        { text: 'Get JSON Web Tokens (JWT)', link: './tutorials/01-50-expose-and-secure-a-workload/v1beta1-deprecated/01-51-get-jwt.md' },
-        { text: 'Secure a Workload with JWT', link: './tutorials/01-50-expose-and-secure-a-workload/v1beta1-deprecated/01-52-expose-and-secure-workload-jwt.md' },
-        { text: 'Secure a Workload with Istio', link: './tutorials/01-50-expose-and-secure-a-workload/v1beta1-deprecated/01-53-expose-and-secure-workload-istio.md' }
-      ]},
+      { text: 'Get JSON Web Tokens (JWT)', link: './tutorials/01-50-expose-and-secure-a-workload/01-51-get-jwt.md' },
+      { text: 'Secure a Workload with JWT', link: './tutorials/01-50-expose-and-secure-a-workload/01-52-expose-and-secure-workload-jwt.md' },
+      { text: 'Secure a Workload with extAuth', link: './tutorials/01-50-expose-and-secure-a-workload/01-53-expose-and-secure-workload-ext-auth.md' },
       { text: 'Secure a Workload with a Certificate', link: './tutorials/01-50-expose-and-secure-a-workload/01-54-expose-and-secure-workload-with-certificate.md' },
       { text: 'Configure IP-Based Access with XFF', link: './tutorials/01-50-expose-and-secure-a-workload/01-55-ip-based-access-with-xff.md' }
     ]},
@@ -57,24 +30,15 @@ export default [
       { text: 'Oathkeeper Dependency', link: './custom-resources/apigateway/04-20-oathkeeper.md' }
     ]},
     { text: 'APIRule Custom Resource', link: './custom-resources/apirule/README.md', collapsed: true, items: [
-      { text: 'v2', link: './custom-resources/apirule/04-10-apirule-custom-resource.md', collapsed: true, items: [
+        { text: 'APIRule v2 Custom Resource', link: './custom-resources/apirule/04-10-apirule-custom-resource.md' },
         { text: 'APIRule Access Strategies', link: './custom-resources/apirule/04-15-api-rule-access-strategies.md' },
-      ]},
-      { text: 'v2alpha1', link: './custom-resources/apirule/v2alpha1/04-10-apirule-custom-resource.md', collapsed: true, items: [
-        { text: 'APIRule Access Strategies', link: './custom-resources/apirule/v2alpha1/04-15-api-rule-access-strategies.md' },
-      ]},
-      { text: 'v1beta1', link: './custom-resources/apirule/v1beta1-deprecated/04-10-apirule-custom-resource.md', collapsed: true, items: [
-        { text: 'Istio JWT Access Strategy', link: './custom-resources/apirule/v1beta1-deprecated/04-20-apirule-istio-jwt-access-strategy.md' },
-        { text: 'Comparison of Access Strategies', link: './custom-resources/apirule/v1beta1-deprecated/04-30-apirule-jwt-ory-and-istio-comparison.md' },
-        { text: 'APIRule Mutators', link: './custom-resources/apirule/v1beta1-deprecated/04-40-apirule-mutators.md' },
-        { text: 'APIRule Authorizations', link: './custom-resources/apirule/v1beta1-deprecated/04-50-apirule-authorizations.md' }
-      ]},
+        { text: 'Ordering Rules in APIRule v2', link: './custom-resources/apirule/04-20-significance-of-rule-path-and-method-order.md' }
     ]},
     { text: 'RateLimit Custom Resource', link: './custom-resources/ratelimit/04-00-ratelimit.md' },
   ]},
   { text: 'APIRule Migration', link: './apirule-migration/README.md', collapsed: true, items: [
     { text: 'Retrieve v1beta1 spec', link: './apirule-migration/01-81-retrieve-v1beta1-spec.md' },
-    { text: 'Migrate Multiple APIRules Targeting the Same Workload from v1beta1 to v2', link: './apirule-migration/01-90-migrate-multiple-apirules-targeting-same-workload.md' },
+    { text: 'Migrate Multiple APIRules Targeting the Same Workload', link: './apirule-migration/01-90-migrate-multiple-apirules-targeting-same-workload.md' },
     { text: 'Migrate jwt Handlers', link: './apirule-migration/01-83-migrate-jwt-v1beta1-to-v2.md' },
     { text: 'Migrate noop, no_auth, allow Handlers', link: './apirule-migration/01-82-migrate-allow-noop-no_auth-v1beta1-to-v2.md' },
     { text: 'Migrate Ory-based Handlers', link: './apirule-migration/01-84-migrate-oauth2-v1beta1-to-v2.md' },
@@ -87,35 +51,19 @@ export default [
     { text: 'Api Gateway Request Flow', link: './technical-reference/05-10-api-gateway-request-flow.md' }
   ]},
   { text: 'Troubleshooting Guides', link: './troubleshooting-guides/README.md', collapsed: true, items: [
-    { text: 'Certificate Issuer Not Created', link: './troubleshooting-guides/03-20-cert-mgt-issuer-not-created.md' },
+    { text: 'Issues When Creating APIRule', link: './troubleshooting-guides/03-40-api-rule-troubleshooting.md' },
+    { text: 'Blocked In-Cluster Communication', link: './troubleshooting-guides/03-80-blocked-in-cluster-communication.md' },
+    { text: 'Missing Rules in APIRule `v2alpha1`', link: './troubleshooting-guides/03-84-missing-rules-apirule-v2alpha1.md' },
+    { text: 'Missing Rules in APIRule `v2`', link: './troubleshooting-guides/03-81-missing-rules-apirule-v2.md' },
+    { text: 'APIRules Missing from Kyma Dashboard', link: './troubleshooting-guides/03-83-apirules-are-missing-from-kyma-dashboard.md' },
+    { text: 'Changed Status Schema in APIRule `v2`', link: './troubleshooting-guides/03-82-changed-status-schema-apirule-v2.md' },
+    { text: 'APIRule Conversion Webhook Failed', link: './troubleshooting-guides/03-60-apirule-conversion-webhook-cert-issue.md' },
+    { text: 'Certificate Management: Issuer Not Created', link: './troubleshooting-guides/03-20-cert-mgt-issuer-not-created.md' },
     { text: 'Kyma Gateway Not Reachable', link: './troubleshooting-guides/03-30-gateway-not-reachable.md' },
+    { text: '401 Unauthorized or 403 Forbidden', link: './troubleshooting-guides/03-01-401-unauthorized-403-forbidden.md' },
+    { text: 'Connection Refused or Timeout', link: './troubleshooting-guides/03-10-dns-mgt-connection-refused.md' },
+    { text: 'Could not Resolve Host', link: './troubleshooting-guides/03-11-dns-mgt-could-not-resolve-host.md' },
+    { text: 'Resource Ignored by the Controller', link: './troubleshooting-guides/03-12-dns-mgt-resource-ignored.md' },
     { text: 'Issues with Gardener Certificates', link: './troubleshooting-guides/03-50-certificates-gardener.md' },
-    { text: 'APIRule and Service Connection Issues', link: './troubleshooting-guides/03-00-cannot-connect-to-service/README.md', collapsed: true, items:[
-      { text: 'APIRule v2', link: './troubleshooting-guides/03-00-cannot-connect-to-service/03-00-basic-diagnostics.md', collapsed: true, items: [
-        { text: 'Issues When Creating APIRule', link: './troubleshooting-guides/03-00-cannot-connect-to-service/03-40-api-rule-troubleshooting.md' },
-        { text: '401 Unauthorized or 403 Forbidden', link: './troubleshooting-guides/03-00-cannot-connect-to-service/03-01-401-unauthorized-403-forbidden.md' }
-      ]},
-      { text: 'APIRule v2alpha1', link: './troubleshooting-guides/03-00-cannot-connect-to-service/v2alpha1/03-00-basic-diagnostics.md', collapsed: true, items: [
-        { text: 'Issues When Creating APIRule', link: './troubleshooting-guides/03-00-cannot-connect-to-service/v2alpha1/03-40-api-rule-troubleshooting.md' },
-        { text: '401 Unauthorized or 403 Forbidden', link: './troubleshooting-guides/03-00-cannot-connect-to-service/v2alpha1/03-01-401-unauthorized-403-forbidden.md' },
-      ]},
-      { text: 'APIRule v1beta1', link: './troubleshooting-guides/03-00-cannot-connect-to-service/v1beta1-deprecated/03-00-basic-diagnostics.md' , collapsed: true, items: [
-        { text: 'Issues When Creating APIRule', link: './troubleshooting-guides/03-00-cannot-connect-to-service/v1beta1-deprecated/03-40-api-rule-troubleshooting.md' },
-        { text: '401 Unauthorized or 403 Forbidden', link: './troubleshooting-guides/03-00-cannot-connect-to-service/v1beta1-deprecated/03-01-401-unauthorized-403-forbidden.md' },
-        { text: '404 Not Found', link: './troubleshooting-guides/03-00-cannot-connect-to-service/v1beta1-deprecated/03-02-404-not-found.md' },
-        { text: '500 Server Error', link: './troubleshooting-guides/03-00-cannot-connect-to-service/v1beta1-deprecated/03-03-500-server-error.md' }
-      ]}
-    ]}
-  ]},
-  { text: 'External DNS Management Errors', link: './troubleshooting-guides/03-10-dns-mgt/README.md' , collapsed: true, items: [
-    { text: 'Connection Refused or Timeout', link: './troubleshooting-guides/03-10-dns-mgt/03-10-dns-mgt-connection-refused.md' },
-    { text: "Couldn't Resolve Host", link: './troubleshooting-guides/03-10-dns-mgt/03-11-dns-mgt-could-not-resolve-host.md' },
-    { text: 'Resource Ignored by the Controller', link: './troubleshooting-guides/03-10-dns-mgt/03-12-dns-mgt-resource-ignored.md' }
-  ]},
-  { text: 'APIRule v2 Introduction', link: './troubleshooting-guides/03-80-apirule-v2-introduction/README.md' , collapsed: true, items: [
-    { text: 'Blocked In-Cluster Communication', link: './troubleshooting-guides/03-80-apirule-v2-introduction/03-80-blocked-in-cluster-communication.md' },
-    { text: 'Missing Rules in APIRule `v2alpha1`', link: './troubleshooting-guides/03-80-apirule-v2-introduction/03-84-missing-rules-apirule-v2alpha1.md' },
-    { text: 'Missing Rules in APIRule `v2`', link: './troubleshooting-guides/03-80-apirule-v2-introduction/03-81-missing-rules-apirule-v2.md' },
-    { text: 'Changed Status Schema in APIRule `v2`', link: './troubleshooting-guides/03-80-apirule-v2-introduction/03-82-changed-status-schema-apirule-v2.md' }
   ]}
 ]
