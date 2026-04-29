7266aa3189828355459f3b030ec7bb2f80008109
Patryk Strugacz <patryk.strugacz@sap.com>
2026-04-29 08:44:52 +0200

    Add more tests, rename function

--- internal/helpers/hosts_test.go
+++ internal/helpers/hosts_test.go
@@ -8,74 +8,89 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("IsFqdnHostName", func() {
+var _ = Describe("IsFqdnOrWildcardHostName", func() {
 	It("Should be false if host is empty", func() {
-		Expect(IsFqdnHostName("")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("")).To(BeFalse())
 	})
 
 	It("Should be true if host is a valid FQDN", func() {
-		Expect(IsFqdnHostName("host.example.com")).To(BeTrue())
+		Expect(IsFqdnOrWildcardHostName("host.example.com")).To(BeTrue())
 	})
 
 	It("Should be true if host is a valid FQDN with 1 char labels", func() {
-		Expect(IsFqdnHostName("a.b.ca")).To(BeTrue())
+		Expect(IsFqdnOrWildcardHostName("a.b.ca")).To(BeTrue())
 	})
 
 	It("Should be false if host name has uppercase letters", func() {
-		Expect(IsFqdnHostName("host.exaMple.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.exaMple.com")).To(BeFalse())
 	})
 
 	It("Should be true if host name has 255 chars", func() {
-		Expect(IsFqdnHostName(fmt.Sprintf("%s.%s.%s.%s.eee", strings.Repeat("a", 63), strings.Repeat("b", 63),
+		Expect(IsFqdnOrWildcardHostName(fmt.Sprintf("%s.%s.%s.%s.eee", strings.Repeat("a", 63), strings.Repeat("b", 63),
 			strings.Repeat("c", 63), strings.Repeat("d", 59)))).To(BeTrue())
 	})
 
 	It("Should be false if host name is longer than 255 chars", func() {
-		Expect(IsFqdnHostName(fmt.Sprintf("%s.%s.%s.%s.eee", strings.Repeat("a", 63), strings.Repeat("b", 63),
+		Expect(IsFqdnOrWildcardHostName(fmt.Sprintf("%s.%s.%s.%s.eee", strings.Repeat("a", 63), strings.Repeat("b", 63),
 			strings.Repeat("c", 63), strings.Repeat("d", 60)))).To(BeFalse())
 	})
 
 	It("Should be false if any domain label is longer than 63 chars", func() {
-		Expect(IsFqdnHostName(fmt.Sprintf("%s.com", strings.Repeat("a", 64)))).To(BeFalse())
-		Expect(IsFqdnHostName(fmt.Sprintf("host.%s.com", strings.Repeat("a", 64)))).To(BeFalse())
-		Expect(IsFqdnHostName(fmt.Sprintf("host.example.%s", strings.Repeat("a", 64)))).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName(fmt.Sprintf("%s.com", strings.Repeat("a", 64)))).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName(fmt.Sprintf("host.%s.com", strings.Repeat("a", 64)))).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName(fmt.Sprintf("host.example.%s", strings.Repeat("a", 64)))).To(BeFalse())
 	})
 
 	It("Should be false if any domain label is empty", func() {
-		Expect(IsFqdnHostName("host.")).To(BeFalse())
-		Expect(IsFqdnHostName(".com")).To(BeFalse())
-		Expect(IsFqdnHostName(".example.com")).To(BeFalse())
-		Expect(IsFqdnHostName("host..com")).To(BeFalse())
-		Expect(IsFqdnHostName("host.example.")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName(".com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName(".example.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host..com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.example.")).To(BeFalse())
 	})
 
 	It("Should be false if top level domain is too short", func() {
-		Expect(IsFqdnHostName("host.example.c")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.example.c")).To(BeFalse())
 	})
 
 	It("Should be false if any domain label contain wrong characters", func() {
-		Expect(IsFqdnHostName("*host.example.com")).To(BeFalse())
-		Expect(IsFqdnHostName("ho*st.example.com")).To(BeFalse())
-		Expect(IsFqdnHostName("host*.example.com")).To(BeFalse())
-		Expect(IsFqdnHostName("host.*example.com")).To(BeFalse())
-		Expect(IsFqdnHostName("host.exam*ple.com")).To(BeFalse())
-		Expect(IsFqdnHostName("host.example*.com")).To(BeFalse())
-		Expect(IsFqdnHostName("host.example.*com")).To(BeFalse())
-		Expect(IsFqdnHostName("host.example.co*m")).To(BeFalse())
-		Expect(IsFqdnHostName("host.example.com*")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("*host.example.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("ho*st.example.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host*.example.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.*example.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.exam*ple.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.example*.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.example.*com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.example.co*m")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.example.com*")).To(BeFalse())
+	})
+
+	It("Should be true if host is a valid wildcard domain", func() {
+		Expect(IsFqdnOrWildcardHostName("*.example.com")).To(BeTrue())
+		Expect(IsFqdnOrWildcardHostName("*.sub.example.com")).To(BeTrue())
+		Expect(IsFqdnOrWildcardHostName("*.local.kyma.dev")).To(BeTrue())
+	})
+
+	It("Should be false if wildcard domain is invalid", func() {
+		Expect(IsFqdnOrWildcardHostName("*.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("*.c")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("*example.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("*.")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("*.example.")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("**.example.com")).To(BeFalse())
 	})
 
 	It("Should be false if any segment in host name starts or ends with hyphen", func() {
-		Expect(IsFqdnHostName("-host.example.com")).To(BeFalse())
-		Expect(IsFqdnHostName("host-.example.com")).To(BeFalse())
-		Expect(IsFqdnHostName("host.-example.com")).To(BeFalse())
-		Expect(IsFqdnHostName("host.example-.com")).To(BeFalse())
-		Expect(IsFqdnHostName("host.example.-com")).To(BeFalse())
-		Expect(IsFqdnHostName("host.example.com-")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("-host.example.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host-.example.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.-example.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.example-.com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.example.-com")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("host.example.com-")).To(BeFalse())
 	})
 
 	It("Should be false if top level domain is too short", func() {
-		Expect(IsFqdnHostName("example.c")).To(BeFalse())
+		Expect(IsFqdnOrWildcardHostName("example.c")).To(BeFalse())
 	})
 })
 
