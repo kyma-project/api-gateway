cb54d002391da0583451789613000de96413266c
Natalia Sitko <natalia.sitko@sap.com>
2026-05-06 13:06:09 +0200

    add missing fields

--- apis/gateway/external/v1alpha1/external_gateway_types.go
+++ apis/gateway/external/v1alpha1/external_gateway_types.go
@@ -120,9 +120,9 @@ type ExternalGatewayStatus struct {
 type ExternalGateway struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	
+
 	// ExternalGatewaySpec defines the desired state of ExternalGateway.
-	Spec   ExternalGatewaySpec   `json:"spec,omitempty"`
+	Spec ExternalGatewaySpec `json:"spec,omitempty"`
 	// ExternalGatewayStatus defines the observed state of an ExternalGateway.
 	Status ExternalGatewayStatus `json:"status,omitempty"`
 }
