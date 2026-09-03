91e24349496b1b24a059100e23768ed9b9375a9d
Tomasz Skrzyp <tomasz.skrzyp@sap.com>
2026-09-03 17:45:36 +0200

    fix: after merge

--- tests/e2e/tests/custom_domain/custom_domain_test.go
+++ tests/e2e/tests/custom_domain/custom_domain_test.go
@@ -23,7 +23,7 @@ import (
 	e2eclient "github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/client"
 	customdomainhelper "github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/customdomain"
 	httpbinhelper "github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/httpbin"
-	"github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/modules"
+	modulehelpers "github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/modules"
 	oauth2mock "github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/oauth2/mock"
 
 	"github.com/avast/retry-go/v4"
