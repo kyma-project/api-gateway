3ba9ecc5718a251ff998bceed890bd89b305003c
Natalia Sitko <80401180+nataliasitko@users.noreply.github.com>
2026-09-15 15:38:00 +0200

    Update _sidebar.ts (#2863)

--- docs/user/_sidebar.ts
+++ docs/user/_sidebar.ts
@@ -25,7 +25,7 @@ export default [
     { text: 'Using a Wildcard Host', link: './expose-workloads/wildcard-host-name.md' },
     { text: 'Configuring IP-Based Access with XFF', link: './expose-workloads/ip-based-access-with-xff.md' },
   ]},
-  { text: 'Rate Limiting', link: './ratelimit/README.md/README.md', collapsed: true, items: [
+  { text: 'Rate Limiting', link: './ratelimit/README.md', collapsed: true, items: [
     { text: 'Configure Local Rate Limiting', link: './ratelimit/configure-local-rate-limit.md' }
   ]},
   { text: 'Network Policies', link: './networkpolicies.md' },
